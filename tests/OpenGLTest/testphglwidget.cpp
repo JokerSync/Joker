12a3a3cf519c6db64658c840d2404b881666fc15
ThomasAy <thomas@dubware.net>
2013-05-13 15:35:01 +0200

    working with Martin's texture

--- tests/OpenGLTest/testphglwidget.cpp
+++ tests/OpenGLTest/testphglwidget.cpp
@@ -9,20 +9,51 @@ TestPhGLWidget::TestPhGLWidget(QWidget *parent)
 
 void TestPhGLWidget::initializeGL()
 {
-
+    /*
+    // Ma tambouille
     f_x += 0.0;
     loadTexture("../../../../../data/box.png");
 
     glEnable(GL_TEXTURE_2D);
     glShadeModel(GL_SMOOTH);
 
-    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
     glClearDepth(1.0f);
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
 
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST);
+    */
+
+    // La tambouille de Martin
+    GLubyte Texture[16] =
+    {
+        0,0,0,0, 0xFF,0xFF,0xFF,0xFF,
+        0xFF,0xFF,0xFF,0xFF, 0,0,0,0
+    };
+    //Image (2x2)
+    GLuint Nom;
+
+    glClearColor(.5,.5,.5,0); 	//Change la couleur du fond
+    glEnable(GL_DEPTH_TEST); 	//Active le depth test
+    glEnable(GL_TEXTURE_2D); 	//Active le texturing
+    glGenTextures(1,&Nom); 	//Génère un n° de texture
+    glBindTexture(GL_TEXTURE_2D,Nom); 	//Sélectionne ce n°
+    glTexImage2D (
+                GL_TEXTURE_2D, 	//Type : texture 2D
+                0, 	//Mipmap : aucun
+                4, 	//Couleurs : 4
+                2, 	//Largeur : 2
+                2, 	//Hauteur : 2
+                0, 	//Largeur du bord : 0
+                GL_RGBA, 	//Format : RGBA
+                GL_UNSIGNED_BYTE, 	//Type des couleurs
+                Texture 	//Addresse de l'image
+                );
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
 
 }
 
@@ -40,48 +71,100 @@ void TestPhGLWidget::resizeGL(int width, int height)
 
 void TestPhGLWidget::paintGL()
 {
-    f_x += 0.1;
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //    f_x += 0.3;
+    //    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //    glLoadIdentity();
+    //    glTranslatef(0.0f, 0.0f, -10.0f);
+    //    glRotatef(f_x, 1.0, 0.3, 0.1);
+
+    //    glBindTexture(GL_TEXTURE_2D, texture[0]);
+
+    //    glBegin(GL_QUADS);
+    //    // Face Avant
+    //    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+    //    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+    //    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
+    //    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
+    //    // Face Arrière
+    //    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+    //    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+    //    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+    //    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+    //    // Face Haut
+    //    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+    //    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
+    //    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
+    //    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+    //    // Face Bas
+    //    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+    //    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+    //    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+    //    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+    //    // Face Droite
+    //    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
+    //    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
+    //    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
+    //    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+    //    // Face Gauche
+    //    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
+    //    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+    //    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
+    //    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+    //    glEnd();
+
+    f_x += 1.6;
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT ); 	//Efface le framebuffer et le depthbuffer
+    glMatrixMode(GL_MODELVIEW); 	//Un petit gluLookAt()...
     glLoadIdentity();
-    glTranslatef(-1.5f, 0.0f, -6.0f);
-    glRotatef(f_x, 1.0, 0.3, 0.1);
+    glTranslatef(0.0f, 0.0f, -15.0f);
+    glRotatef(f_x, 1, 1, 0.1);
 
-    glBindTexture(GL_TEXTURE_2D, texture[0]);
+    gluLookAt(3,2,3,0,0,0,0,1,0);
 
-    glBegin(GL_QUADS);
-    // Face Avant
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
-    // Face Arrière
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
-    // Face Haut
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
-    // Face Bas
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
-    // Face Droite
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, -1.0f, -1.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 1.0f,  1.0f, -1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f( 1.0f,  1.0f,  1.0f);
-    glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
-    // Face Gauche
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, -1.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f(-1.0f,  1.0f,  1.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-1.0f,  1.0f, -1.0f);
+    glBegin(GL_QUADS); 	//Et c'est parti pour le cube !
+
+    glTexCoord2i(0,0);glVertex3i(-1,-1,-1);
+    glTexCoord2i(1,0);glVertex3i(+1,-1,-1);
+    glTexCoord2i(1,1);glVertex3i(+1,+1,-1);
+    glTexCoord2i(0,1);glVertex3i(-1,+1,-1);
+
+    //1 face
+
+    glTexCoord2i(0,0);glVertex3i(-1,-1,+1);
+    glTexCoord2i(1,0);glVertex3i(+1,-1,+1);
+    glTexCoord2i(1,1);glVertex3i(+1,+1,+1);
+    glTexCoord2i(0,1);glVertex3i(-1,+1,+1);
+
+    //2 faces
+
+    glTexCoord2i(0,0);glVertex3i(+1,-1,-1);
+    glTexCoord2i(1,0);glVertex3i(+1,-1,+1);
+    glTexCoord2i(1,1);glVertex3i(+1,+1,+1);
+    glTexCoord2i(0,1);glVertex3i(+1,+1,-1);
+
+    //3 faces
+
+    glTexCoord2i(0,0);glVertex3i(-1,-1,-1);
+    glTexCoord2i(1,0);glVertex3i(-1,-1,+1);
+    glTexCoord2i(1,1);glVertex3i(-1,+1,+1);
+    glTexCoord2i(0,1);glVertex3i(-1,+1,-1);
+
+    //4 faces
+
+    glTexCoord2i(1,0);glVertex3i(-1,+1,-1);
+    glTexCoord2i(1,1);glVertex3i(+1,+1,-1);
+    glTexCoord2i(0,1);glVertex3i(+1,+1,+1);
+    glTexCoord2i(0,0);glVertex3i(-1,+1,+1);
+
+    //5 faces
+
+    glTexCoord2i(1,0);glVertex3i(-1,-1,+1);
+    glTexCoord2i(1,1);glVertex3i(+1,-1,+1);
+    glTexCoord2i(0,1);glVertex3i(+1,-1,-1);
+    glTexCoord2i(0,0);glVertex3i(-1,-1,-1);
+
+    //6 faces
     glEnd();
-
-
 
 }
 
