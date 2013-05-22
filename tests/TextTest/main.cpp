2be0e2a75c1f7e3be295cd555087cfb1618f51b8
Martin Delille <martin.delille@gmail.com>
2013-05-22 15:53:53 +0200

    renaming TextTest.* => TextTestWindow.*

--- tests/TextTest/main.cpp
+++ tests/TextTest/main.cpp
@@ -1,11 +1,12 @@
-#include "TextTest.h"
+#include "TextTestWindow.h"
 #include "SDL/SDL.h"
 
 int main( int argc, char* argv[] )
 {
     QApplication a(argc, argv);
-    TextTest w ;
+    TextTestWindow w ;
     w.show();
-    return a.exec();
 
+    return a.exec();
 }
+
