eae2e9d1c7322b23d267913ea211f918c4833c94
Martin Delille <martin.delille@gmail.com>
2013-05-22 16:06:12 +0200

    change color of the text

--- tests/TextTest/SDLMain.cpp
+++ tests/TextTest/SDLMain.cpp
@@ -20,7 +20,6 @@ int main (int argc, char **argv)
     int    gArgc;
     char  **gArgv;
 
-    /* Copy the arguments into a global variable */
     /* This is passed if we are launched by double-clicking */
     if ( argc >= 2 && strncmp (argv[1], "-psn", 4) == 0 ) {
         gArgv = (char **) SDL_malloc(sizeof (char *) * 2);
