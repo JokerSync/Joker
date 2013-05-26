91950d7e8d1cb3902554a9ca9f0a093b3ee0a071
ThomasAy <thomas@dubware.net>
2013-05-26 17:28:40 +0200

    Continue the implementation of PhPeople class

--- libs/PhStrip/PhPeople.cpp
+++ libs/PhStrip/PhPeople.cpp
@@ -11,3 +11,14 @@ PhPeople::PhPeople(PhString name, PhColor color)
     _name = name;
     _color = color;
 }
+
+PhString PhPeople::getName()
+{
+    return _name;
+}
+
+
+PhColor PhPeople::getColor()
+{
+    return _color;
+}
