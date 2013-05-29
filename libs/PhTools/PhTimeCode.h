2a0853167ccbb520d6232f42eb339b8741c08bbb
Martin Delille <martin.delille@gmail.com>
2013-05-29 22:08:30 +0200

    adding PhTimeCode

--- libs/PhTools/PhTimeCode.h
+++ libs/PhTools/PhTimeCode.h
@@ -1,11 +1,100 @@
 #ifndef PHTIMECODE_H
 #define PHTIMECODE_H
 
-#define PhTimeCode float
-//class PhTimeCode
-//{
-//public:
-//    PhTimeCode();
-//};
+#include "PhString.h"
+
+/**
+ Type for the timecode frame.
+ */
+typedef long PhFrame;
+
+/** Different type of timecode
+
+ Enumerate the different type of timecode supported by the system.
+ */
+typedef enum {
+    PhTimeCodeType2398,
+    PhTimeCodeType24,
+    PhTimeCodeType25,
+    PhTimeCodeType2997
+} PhTimeCodeType;
+
+
+/** Timecode representation utility
+
+ Provide tools for converting between frame, string representation and
+ BCD representation of a timecode value.
+ */
+class PhTimeCode
+{
+public:
+    /**
+     Create a timecode string representation from a frame number and a type
+     @param frame A frame number.
+     @param type A PhTimeCodeType value.
+     @return An NSString* timecode representation.
+     */
+    static PhString stringFromFrame(PhFrame frame, PhTimeCodeType type);
+
+    /**
+     Compute the frame number from a timecode string representation and a type
+     @param string A string.
+     @param type A PhTimeCodeType value.
+     @return The corresponding frame number.
+     */
+    static PhFrame frameFromString(PhString string, PhTimeCodeType type);
+
+    /**
+     Compute the frame number from a timecode binary coded decimal (BCD) representation and a type
+     @param bcd A BCD value.
+     @param type A PhTimeCodeType value.
+     @return The corresponding frame number.
+     */
+    static PhFrame frameFromBcd(unsigned int bcd, PhTimeCodeType type);
+
+    /**
+     Create a timecode BCD representation from a frame number and a type
+     @param frame A frame number.
+     @param type A PhTimeCodeType value.
+     @return A timecode BCD representation.
+     */
+    static unsigned int bcdFromFrame(PhFrame frame, PhTimeCodeType type);
+
+    /**
+     Compute the HH, MM, SS and FF timecode component from a frame number and a type.
+     @param hh Hours output value
+     @param mm Minutes output value
+     @param ss Secondes output value
+     @param ff Frame output value
+     @param frame Frame number
+     @param type Timecode typ
+     */
+    static void ComputeHhMmSsFf(unsigned int *hhmmssff, PhFrame frame, PhTimeCodeType type);
+
+    /**
+     Compute frame number from the HH, MM, SS and FF timecode component and a type.
+     @param hh Hours output value
+     @param mm Minutes output value
+     @param ss Secondes output value
+     @param ff Frame output value
+     @param type Timecode typ
+     @return A frame number
+     */
+    static PhFrame frameFromHhMmSsFf(unsigned int * hhmmssff, PhTimeCodeType type);
+
+    /**
+     * @brief check if the timecode type is dropframe
+     * @param type of the timecode
+     * @return if the timecode type is dropframe
+     */
+    static bool isDrop(PhTimeCodeType type);
+
+    /**
+     * @brief check if the timecode type frame per second
+     * @param type of the timecode
+     * @return amount of frame per second
+     */
+    static PhFrame getFps(PhTimeCodeType type);
+};
 
 #endif // PHTIMECODE_H
---
+++ tests/ConsoleTest/main.cpp
@@ -0,0 +1,20 @@
+#include <QCoreApplication>
+
+#include <QDebug>
+
+#include "PhTools/PhTimeCode.h"
+
+int main(int argc, char *argv[])
+{
+    //QCoreApplication a(argc, argv);
+
+    for(int i=0; i<2000000;i++)
+    {
+        PhString s = PhTimeCode::stringFromFrame(i, PhTimeCodeType25);
+        int n = PhTimeCode::frameFromString(s, PhTimeCodeType25);
+        if(n != i)
+            qDebug() << "problem with " << i;
+    }
+    //return a.exec();
+    return 0;
+}
