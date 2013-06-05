ff21e150288eb7d9ce44003d83b0e6a5dc9ed940
ThomasAy <thomas@dubware.net>
2013-06-05 09:45:57 +0200

    adding license

--- libs/PhStrip/PhStripCut.cpp
+++ libs/PhStrip/PhStripCut.cpp
@@ -1,3 +1,8 @@
+/**
+* Copyright (C) 2012-2013 Phonations
+* License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+*/
+
 #include "PhStripCut.h"
 
 PhStripCut::PhStripCut()
--- libs/PhTools/memorytool.cpp
+++ libs/PhTools/memorytool.cpp
@@ -1,6 +1,12 @@
-#include "memorytool.h"
+/**
+* Copyright (C) 2012-2013 Phonations
+* License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+*/
 #include <QDebug>
 #include <QString>
+
+#include "memorytool.h"
+
 
 void MemoryDump(void * pixels, int w, int h, int bpp)
 {
