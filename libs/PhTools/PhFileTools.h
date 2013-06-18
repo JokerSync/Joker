7a89a2618df6bac32e8b7777d331e45c1b8d91d7
ThomasAy <thomas@dubware.net>
2013-06-18 17:43:06 +0200

    framework copy setting

---
+++ libs/PhTools/PhFileTools.h
@@ -0,0 +1,15 @@
+/**
+* Copyright (C) 2012-2013 Phonations
+* License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+*/
+
+#ifndef PHFILETOOLS_H
+#define PHFILETOOLS_H
+
+#include <QDir>
+#include <QFile>
+
+#include "PhString.h"
+
+void copyFolder(PhString sourceFolder, PhString destFolder);
+#endif // PHFILETOOLS_H
