931d07191f8b4d55f88c111b4187f4053b6e2523
ThomasAy <thomas@dubware.net>
2013-06-28 18:32:48 +0200

    Adding leap

---
+++ libs/PhCommonUI/SampleListener.h
@@ -0,0 +1,14 @@
+#include "Leap.h"
+
+using namespace Leap;
+
+class SampleListener : public Listener {
+  public:
+    virtual void onInit(const Controller&);
+    virtual void onConnect(const Controller&);
+    virtual void onDisconnect(const Controller&);
+    virtual void onExit(const Controller&);
+    virtual void onFrame(const Controller&);
+    virtual void onFocusGained(const Controller&);
+    virtual void onFocusLost(const Controller&);
+};
