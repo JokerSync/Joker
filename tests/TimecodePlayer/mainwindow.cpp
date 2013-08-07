a5970e7d2a148aed4ce47062726a2e0041433018
julienravit <julien@dubware>
2013-08-07 11:50:28 +0200

    Play/pause button + rate textbox + timecode textbox

--- tests/TimecodePlayer/mainwindow.cpp
+++ tests/TimecodePlayer/mainwindow.cpp
@@ -3,12 +3,79 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    _ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    _ui->setupUi(this);
+    _widthWindow = 600;
+    _heightWindow = 200;
+    resize(_widthWindow,_heightWindow);
+
+    //Model Init
+    _playButtonState = false;
+    _rateValue = 0;
+    _timecodeValue.hours = 0;
+    _timecodeValue.minutes = 0;
+    _timecodeValue.seconds = 0;
+    _timecodeValue.ms = 0;
+
+    //Buttons Init
+
+    _playButton = new QPushButton("", this);
+    _playButton->setGeometry(_widthWindow/2-30,0.75*_heightWindow-30,60,60);
+    _playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+    QObject::connect(_playButton, SIGNAL(clicked()), this, SLOT(changeValuePlayButton()));
+
+    //Label Init
+
+    _rate = new QLabel(QString::number(_rateValue), this);
+    _rate->setGeometry(50+20,_heightWindow/2-25,100,50);
+
+    _timecode = new QLabel("00:00:00:00", this);
+    _timecode->setGeometry((1-1/5)*_widthWindow-100-20,_heightWindow/2-25,200,50);
+
+
+
 }
+
+
+/****************************Slots****************************/
+
+
+void MainWindow::changeValuePlayButton()
+{
+    if(_playButtonState == false)//If pause
+    {
+        _rateValue = 1;
+        _playButtonState = true;
+        _playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
+
+    }
+    else //If playing
+    {
+        _rateValue = 0;
+        _playButtonState = false;
+        _playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+    }
+    _rate->setNum(_rateValue);
+}
+
+/****************************Accessors****************************/
+
+double MainWindow::get_rateValue() const
+{
+    return _rateValue;
+}
+
+
+/****************************Methods****************************/
+
+
+
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+    delete _timecode;
+    delete _playButton;
+    delete _rate;
+    delete _ui;
 }
--- tests/TimecodePlayer/mainwindow.h
+++ tests/TimecodePlayer/mainwindow.h
@@ -5,34 +5,97 @@
 #include <QMainWindow>
 #include <QtWidgets>
 #include <QString>
+//#include <Qtimer>
 
 
 namespace Ui {
 class MainWindow;
 }
 
+typedef struct Timecode Timecode;
+struct Timecode{
+    int hours;
+    int minutes;
+    int seconds;
+    int ms;
+};
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(int x, int y, QWidget *parent = 0);
+
+    explicit MainWindow(QWidget *parent = 0);
+
+    //Accessors
+    /**
+     * @brief get_rateValue
+     * @return _rateValue
+     */
+    double get_rateValue() const;
+
+    //Methods
+
+
     ~MainWindow();
 
+
 public slots:
+    /**
+     * @brief changeValuePlayButton
+     * Change the state of _playButtonState when cliking on _plauButton
+     */
+    void changeValuePlayButton();
 
 
 private:
     Ui::MainWindow *_ui;
-    //Buttons
+
+    //Controller section
+    /**
+     * @brief _playButton
+     */
     QPushButton *_playButton;// play/pause button
-    QPushButton *_stopButton;// stop button + back to the beginning
+  /*  QPushButton *_stopButton;// stop button + back to the beginning
     QPushButton *_fastForwardButton;// fast forward when activated
     QPushButton *_fastRewardButton;// fast reward when activated
-    QLabel *_label;
-    QLabel *_label2;
-    int _valueSlider;
-    bool _valueButton;
+*/
+    //View Section
+    /**
+     * @brief _timecode
+     */
+    QLabel *_timecode;
+    /**
+     * @brief _rate
+     */
+    QLabel *_rate;
+
+
+    //Model Section
+    /**
+     * @brief _heightWindow
+     */
+    int _heightWindow;
+    /**
+     * @brief _widthWindow
+     */
+    int _widthWindow;
+    /**
+     * @brief _playButtonState
+     * true when playing, false when pause
+     */
+    bool _playButtonState;
+    /**
+     * @brief _rateValue
+     * Value of the rate (0=pause, 1=play)
+     */
+    double _rateValue;
+    /**
+     * @brief _timecode
+     */
+    Timecode _timecodeValue;
+
 };
 
 
