769f9ec7de4dea281b3772088bfd304a24ed2dc6
Philippe Eid <philippe@dubware.net>
2013-08-08 12:20:29 +0200

    disable gridLayout

--- tests/GraphicTest/main.cpp
+++ tests/GraphicTest/main.cpp
@@ -6,20 +6,17 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	QWidget w, fenetre;
-	fenetre.resize(400,300);
-	w.resize(350,250);
-	GraphicTestView g(&w, "view");
+	QMainWindow w;
+	w.resize(400,300);
+	GraphicTestView g(&w);
 	g.resize(350,250);
 
-	QGridLayout *gLayout = new QGridLayout;
+//	QGridLayout *gLayout = new QGridLayout;
+//	gLayout->setMargin(0);
+//	gLayout->addWidget(&g, 0, 0);
 
-	gLayout->addWidget(&w, 0, 0);
+//	w.setLayout(gLayout);
+	w.show();
 
-	fenetre.setLayout(gLayout);
-	fenetre.show();
-
-//	w.resize(400,300);
-//	g.resize(380,280);
 	return a.exec();
 }
