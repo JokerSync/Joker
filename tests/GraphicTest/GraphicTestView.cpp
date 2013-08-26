7bb65d818bf5c5308600f8e876cc096214c8ff9b
Philippe Eid <philippe@dubware.net>
2013-08-26 14:12:28 +0200

    add checkbox for display

--- tests/GraphicTest/GraphicTestView.cpp
+++ tests/GraphicTest/GraphicTestView.cpp
@@ -2,17 +2,8 @@
 #include "math.h"
 
 GraphicTestView::GraphicTestView(QWidget *parent, QString name)
-	: PhGraphicView( parent , name )
-#ifdef IMAGE
-	, _image(NULL)
-#endif
-#ifdef TEXT
-	, _font(NULL)
-	, _text(NULL)
-#endif
-#ifdef RECT
-	, _rect(NULL)
-#endif
+	: PhGraphicView( parent , name ), _image(NULL)
+	, _font(NULL), _text(NULL), _rect(NULL)
 {
 
 }
@@ -21,89 +12,93 @@ bool GraphicTestView::init()
 {
 	playEnable = 0;
 	textSpeed = 0;
+	enableDisplayImage = false;
+	enableDisplayRect = false;
+	enableDisplayText = false;
 
-#ifdef IMAGE
-	qDebug() << "GraphicTestView::init";
-	if(_image == NULL)
-	{
-		qDebug() << "Initialize _image";
-		_image = new PhGraphicImage();
-		_image->setFilename("look.png");
-		_image->setTextureCoordinate(1,1);
-		_image->setRect(50,0,250,125);
-		if (! _image->init())
-			qDebug() << "_image not initialize";
-	}
-#endif
+		qDebug() << "GraphicTestView::init";
+		if(_image == NULL)
+		{
+			qDebug() << "Initialize _image";
+			_image = new PhGraphicImage();
+			_image->setFilename("look.png");
+			_image->setTextureCoordinate(1,1);
+			_image->setRect(50,0,250,125);
+			if (! _image->init())
+				qDebug() << "_image not initialize";
+		}
 
-#ifdef TEXT
-	if(_font == NULL)
-	{
-		qDebug() << "Initialize _font";
-		_font = new PhFont("Bedizen.ttf",50);
-		if (! _font->init())
-			qDebug() << "_font not initialize";
-	}
 
-	if(_text == NULL)
-	{
-		qDebug() << "Initialize _text";
-		_text = new PhGraphicText(_font, "Test PhGraphicText");
-		_text->setRect(50,135,250,125);
-		_text->setColor(new QColor(100, 254, 0));
-		if (! _text->init())
-			qDebug() << "_text not initialize";
 
-		_text->setX(280);
-	}
-#endif
+		if(_font == NULL)
+		{
+			qDebug() << "Initialize _font";
+			_font = new PhFont("Bedizen.ttf",50);
+			if (! _font->init())
+				qDebug() << "_font not initialize";
+		}
 
-#ifdef RECT
-	if(_rect == NULL)
-	{
-		qDebug() << "Initialize _text";
-		_rect = new PhGraphicSolidRect(100, 100, 75, 40, 1, new QColor(200, 128, 0));
-	}
-#endif
+		if(_text == NULL)
+		{
+			qDebug() << "Initialize _text";
+			_text = new PhGraphicText(_font, "Test PhGraphicText");
+			_text->setRect(50,135,250,125);
+			_text->setColor(new QColor(100, 254, 0));
+			if (! _text->init())
+				qDebug() << "_text not initialize";
+
+			_text->setX(280);
+		}
+
+		if(_rect == NULL)
+		{
+			qDebug() << "Initialize _text";
+			_rect = new PhGraphicSolidRect(100, 100, 75, 40, 1, new QColor(200, 128, 0));
+		}
 
 	return true;
 }
 
 void GraphicTestView::paint()
 {
-#ifdef IMAGE
-	qDebug() << "GraphicTestView::paint";
-	if(_image != NULL)
-		_image->draw();
-#endif
-
-#ifdef TEXT
-	if (textSpeed == 0)
+	if  (enableDisplayImage == true)
 	{
-		if(_text != NULL)
+		if(_image != NULL)
 		{
+			qDebug() << "GraphicTestView::draw";
+			_image->draw();
+		}
+	}
+
+	if  (enableDisplayText == true)
+	{
+		if (textSpeed == 0)
+		{
+			if(_text != NULL)
+			{
 				_text->draw();
+			}
 		}
-	}
-	else
-	{
-		if(_text != NULL)
+		else
 		{
-			qDebug() <<"text pas null";
-			_text->setX(_text->getX()+(2*textSpeed));
-			_text->draw();
-			if(_text->getX() > this->width())
-				_text->setX(0);
-			if((_text->getX()+_text->getWidth()) < 0)
-				_text->setX(this->width());
+			if(_text != NULL)
+			{
+				qDebug() <<"text pas null";
+				_text->setX(_text->getX()+(2*textSpeed));
+				_text->draw();
+				if(_text->getX() > this->width())
+					_text->setX(0);
+				if((_text->getX()+_text->getWidth()) < 0)
+					_text->setX(this->width());
 
+			}
 		}
 	}
-#endif
 
-#ifdef RECT
-	_rect->draw();
-#endif
+	if  (enableDisplayRect == true)
+	{
+		_rect->draw();
+	}
 }
 
 
@@ -134,3 +129,29 @@ void GraphicTestView::fastForward()
 	if(fabs(textSpeed) > 8)
 		textSpeed = 8;
 }
+
+void GraphicTestView::displayText()
+{
+	if (enableDisplayText == false)
+		enableDisplayText = true;
+	else
+		enableDisplayText = false;
+}
+
+
+void GraphicTestView::displayImage()
+{
+	if (enableDisplayImage == false)
+		enableDisplayImage = true;
+	else
+		enableDisplayImage = false;
+}
+
+void GraphicTestView::displayRect()
+{
+	if (enableDisplayRect == false)
+		enableDisplayRect = true;
+	else
+		enableDisplayRect = false;
+}
+
