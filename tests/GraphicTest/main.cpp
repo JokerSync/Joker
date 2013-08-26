b32ec23a6c6e3c1fb49ca3a5d71fa2d57661f5d3
Philippe Eid <philippe@dubware.net>
2013-08-26 10:56:40 +0200

    renaming qtDesigner to MainWindow

--- tests/GraphicTest/main.cpp
+++ tests/GraphicTest/main.cpp
@@ -2,22 +2,14 @@
 #include <QtGui>
 #include <QtCore>
 #include "PhGraphicSolidRect.h"
-#include "QtDesigner.h"
-#include "ui_QtDesigner.h"
+#include "MainWindow.h"
 #include "GraphicTestView.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	QtDesigner w;
+	MainWindow w;
 //	w.resize(400,300);
-	GraphicTestView g(&w);
-	g.resize(800,400);
-
-	QObject::connect(w.getUi()->Play, SIGNAL(clicked()), &g, SLOT(play()));
-	QObject::connect(w.getUi()->FastBackward, SIGNAL(clicked()), &g, SLOT(fastBackward()));
-	QObject::connect(w.getUi()->FastForward, SIGNAL(clicked()), &g, SLOT(fastForward()));
-
 	w.show();
 
 	return a.exec();
