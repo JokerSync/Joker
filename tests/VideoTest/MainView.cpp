8c198e17a9e39143a4ad5bfa23252dfdd043cfc6
julienravit <julien@dubware>
2013-08-27 15:45:28 +0200

    Remove PhMediaControllerView from Videotest

--- tests/VideoTest/MainView.cpp
+++ tests/VideoTest/MainView.cpp
@@ -5,41 +5,15 @@
 #include <qvideosurfaceformat.h>
 
 MainView::MainView()
-	: QWidget(0),
-	_positionSlider(0)
+	: QWidget(0)
 {
 	_clock = new PhClock;
-	_timer = new QTimer();
-	_framePerSecond = 25;
-
-	_timer->start(40);
-	_mediaControllerView = new PhMediaControllerView(_clock);
-	qDebug() << "filesize:"<<_videoController.duration();
-	qDebug() << "metadata:"<<_videoController.isMetaDataAvailable();
-
-
 
     // Add an open button
     _openButton = new QPushButton(tr("Open..."));
     // Open a file dialog when user click the open button
     connect(_openButton, SIGNAL(clicked()), this, SLOT(onOpenFile()));
 
-	connect(_mediaControllerView, SIGNAL(useSliderCursorSignal()), this, SLOT(positionChanged()));
-
-    // Update the video position when the user moves the slider
-	//connect(_positionSlider, SIGNAL(sliderMoved(int)), &_videoController, SLOT(updatePositionFromPercentage(int)));
-	connect(_clock, SIGNAL(rateChanged()), this, SLOT(changePlayerState()));
-	connect(_mediaControllerView, SIGNAL(nextFrameButtonSignal()), this, SLOT(pushedNextFrameButton()));
-	connect(_mediaControllerView, SIGNAL(previousFrameButtonSignal()), this, SLOT(pushedPreviousFrameButton()));
-	connect(_mediaControllerView, SIGNAL(backButtonSignal()), this, SLOT(pushedBackButton()));
-
-
-
-    // Add an error label
-    // Update error label in case of video error
-	//connect(&_videoController, SIGNAL(error(QMediaPlayer::Error)), this, SLOT(updateErrorLabelContent()));
-
-	connect(_clock, SIGNAL(rateChanged()), this, SLOT(changePlayerState()));
 
     // Create a first horizontal layout for buttons and slider
     QBoxLayout *controlLayout = new QHBoxLayout;
@@ -51,13 +25,12 @@ MainView::MainView()
     layout->setMargin(0);
 	layout->addWidget(_videoController.view());
     layout->addLayout(controlLayout);
-	layout->addWidget(_mediaControllerView);
 
     // Add the layout to the main window
     this->setLayout(layout);
-	connect(_timer, SIGNAL(timeout()), this, SLOT(updateFrame()));
 	_videoController.play();
 }
+
 
 bool MainView::openFile(QString fileName)
 {
@@ -65,85 +38,15 @@ bool MainView::openFile(QString fileName)
     if (fileInfo.exists())
     {
 		_videoController.open(fileName);
-		_videoController.setPlaybackRate(_clock->getRate());
-		qDebug()<<"duration:"<<_videoController.duration();
+		_videoController.play();
         return true;
     }
 	return false;
 }
 
 
-void MainView::updateFrame()
-{
-	int f;
-	_clock->tick();
-	f = _clock->getFrame();
-	//_videoController.setPosition(f);
-	//qDebug()<<"frame"<<f<<"clock"<< 1000*f/24 <<"position"<< _videoController.position();
-
-}
-
-void MainView::changePlayerState()
-{
-	int r = _clock->getRate();
-
-	if(r == 0)
-	{
-		_videoController.pause();
-	}
-	else
-		_videoController.play();
-
-	_videoController.setPlaybackRate(r);
-
-	qDebug ()<<"rate"<<_videoController.playbackRate();
-}
-
-void MainView::positionChanged()
-{
-	qint64 f = _clock->getFrame();
-	qint64 p = f*1000/_mediaControllerView->getFramePerSecond();
-	_videoController.setPosition(p);
-}
-
-void MainView::pushedNextFrameButton()
-{
-	qint64 position = _videoController.position() + 1000/_framePerSecond;
-	_videoController.setPosition(position);
-	qDebug() << "metadata:"<<_videoController.isMetaDataAvailable();
-	foreach(QString meta, _videoController.availableMetaData())
-		qDebug() << meta;
-}
-
-void MainView::pushedPreviousFrameButton()
-{
-	qint64 position = _videoController.position() - 1000/_framePerSecond;
-	_videoController.setPosition(position);
-}
-
-void MainView::pushedBackButton()
-{
-	_videoController.setPosition(0);
-}
-
-
-
 void MainView::onOpenFile()
 {
-    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Movie"),QDir::homePath());
-    openFile(fileName); // TODO: show error in case of error
+	 QString fileName = QFileDialog::getOpenFileName(this, tr("Open Movie"),QDir::homePath());
+	 openFile(fileName); // TODO: show error in case of error
 }
-
-/*void MainView::updatePlayButtonState(QMediaPlayer::State state)
-{
-    switch(state) {
-    case QMediaPlayer::PlayingState:
-        _playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
-        break;
-    default:
-        _playButton->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
-        break;
-    }
-}*/
-
-
