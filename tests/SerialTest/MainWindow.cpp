724ffc0a1aafd272d685a79c1e2809383b09c953
Martin Delille <martindelille@MacBook-Pro-de-Martin.local>
2013-08-30 09:14:38 +0200

    basic serial test project

---
+++ tests/SerialTest/MainWindow.cpp
@@ -0,0 +1,41 @@
+#include <QDebug>
+#include <QSerialPortInfo>
+
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+	QMainWindow(parent),
+	ui(new Ui::MainWindow),
+	_serial(this)
+{
+	ui->setupUi(this);
+
+	qDebug() << "PhSonyController::open()";
+	foreach(QSerialPortInfo info, QSerialPortInfo::availablePorts())
+	{
+		QString name = info.portName();
+		if(name.startsWith("usbserial-") && name.endsWith("A"))
+		{
+			_serial.setPort(info);
+			qDebug() << "Opening " << name;
+			_serial.open(QSerialPort::ReadWrite);
+			_serial.write("Grand contrôle de mes tétons");
+		}
+	}
+
+
+	connect(ui->sendButton, SIGNAL(clicked()), this, SLOT(sendText()));
+}
+
+MainWindow::~MainWindow()
+{
+	qDebug() << "Closing " << _serial.objectName();
+	_serial.close();
+	delete ui;
+}
+
+void MainWindow::sendText()
+{
+	_serial.write(ui->lineEdit->text().toUtf8().constData());
+}
