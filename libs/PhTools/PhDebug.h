d624dfe0311142403440acd259c0c3707c38083d
ThomasAy <thomas@dubware.net>
2013-09-03 14:58:23 +0200

    First version of PhDebug
    Working with function name

---
+++ libs/PhTools/PhDebug.h
@@ -0,0 +1,14 @@
+/**
+* Copyright (C) 2012-2013 Phonations
+* License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+*/
+
+#ifndef PHDEBUG_H
+#define PHDEBUG_H
+
+#include <QDebug>
+
+#define PHDEBUG  qDebug() << __FUNCTION__ << ":"
+
+
+#endif // PHDEBUG_H
