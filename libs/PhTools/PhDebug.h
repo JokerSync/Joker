898618c426bbbb2d843645c63263344849682e03
ThomasAy <thomas@dubware.net>
2013-09-04 12:28:24 +0200

    Now with timestamp (not fully functionnal - see description)
    Display even if it shouldn't :
    if (false)
        DEBUG << "Hello";
    will display something

--- libs/PhTools/PhDebug.h
+++ libs/PhTools/PhDebug.h
@@ -7,8 +7,31 @@
 #define PHDEBUG_H
 
 #include <QDebug>
+#include <QDate>
+#include <QRect>
+
+#define DEBUG PhDebug d; d <<  PHDEBUG
 
 #define PHDEBUG  qDebug() << __FUNCTION__ << ":"
 
+// In order to get rid of double quotes when displaying a variable
+#define Q(string) (string).toStdString().c_str()
+
+class PhDebug
+{
+public:
+    QDebug operator<<(QDebug dbg)
+    {
+        QString d;
+        d = QDate::currentDate().toString("dd.MM.yyyy");
+        d += " - ";
+        d += QTime::currentTime().toString("hh.mm.ss.zzz");
+        d += " in";
+        dbg << Q(d);
+
+        return dbg;
+    }
+
+};
 
 #endif // PHDEBUG_H
