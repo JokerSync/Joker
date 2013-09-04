8a9fcada69bd6fe6d4094b1e4a119b06b5224495
ThomasAy <thomas@dubware.net>
2013-09-04 12:28:24 +0200

    Now with timestamp (not fully functionnal - see description)
    Display even if it shouldn't :
    if (false)
        DEBUG << "Hello";
    will display something

--- tests/ConsoleTest/main.cpp
+++ tests/ConsoleTest/main.cpp
@@ -3,20 +3,19 @@
 #include "PhTools/PhTimeCode.h"
 #include "PhTools/PhDebug.h"
 
-
 int main(int argc, char *argv[])
 {
     // Test of PhDebug tool
-    PHDEBUG << "coucou";
+    DEBUG << "Thomas est trop fort";
 
     // Test of TimeCode
     for(int i=0; i<3;i++)
     {
         QString s = PhTimeCode::stringFromFrame(i, PhTimeCodeType25);
         int n = PhTimeCode::frameFromString(s, PhTimeCodeType25);
-		qDebug() << s;
-        if(n != i)
-            qDebug() << "problem with " << i;
+        DEBUG << s;
+        if(false)
+            DEBUG << "problem with " << i;
     }
 
     return 0;
