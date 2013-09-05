2006e2e726597116243dc796e38d881056530050
ThomasAy <thomas@dubware.net>
2013-09-05 11:58:48 +0200

    step one with singleton - not working

--- libs/PhTools/PhDebug.cpp
+++ libs/PhTools/PhDebug.cpp
@@ -5,3 +5,16 @@
 
 #include "PhDebug.h"
 
+// Global static pointer used to ensure a single instance of the class.
+PhDebug* PhDebug::d = NULL;
+// This function is called to create an instance of the class.
+// Calling the constructor publicly is not allowed. The constructor
+// is private and is only called by this Instance function.
+
+
+PhDebug* PhDebug::instance()
+{
+   if (!d)   // Only allow one instance of class to be generated.
+      d = new PhDebug;
+   return d;
+}
