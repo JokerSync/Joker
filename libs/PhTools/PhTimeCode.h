1d9db8a4902c649af051f16e1d7039c78f47bced
Martin Delille <martindelille@MacBook-Pro-de-Martin.local>
2013-09-14 17:27:16 +0200

    fix time sense for 00:00:25:00

--- libs/PhTools/PhTimeCode.h
+++ libs/PhTools/PhTimeCode.h
@@ -47,8 +47,6 @@ public:
      */
     static PhFrame frameFromBcd(unsigned int bcd, PhTimeCodeType type);
 
-	static unsigned int shortFromBcd(unsigned int bcd);
-
     /**
      Create a timecode BCD representation from a frame number and a type
      @param frame A frame number.
