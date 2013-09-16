d49d7c1d427ba6151add33c7cca95f6e85877345
julienravit <julienravit@Mac-mini-de-PlayR.local>
2013-09-16 15:35:02 +0200

    Adding a timecode imput

--- tests/FormTest/MainWindow.cpp
+++ tests/FormTest/MainWindow.cpp
@@ -6,9 +6,19 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    _frame = 0;
+    _tcType = PhTimeCodeType25;
+    ui->timeCodeLabel->setText(PhTimeCode::stringFromFrame(_frame, _tcType));
+
+    connect(ui->timeCodeInput, SIGNAL(frameChanged(PhFrame)), this, SLOT(updateText(PhFrame)));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::updateText(PhFrame frame)
+{
+    ui->timeCodeLabel->setText(PhTimeCode::stringFromFrame(frame, _tcType));
 }
--- tests/FormTest/TimeCodeInput.cpp
+++ tests/FormTest/TimeCodeInput.cpp
@@ -6,9 +6,28 @@ TimeCodeInput::TimeCodeInput(QWidget *parent) :
     ui(new Ui::TimeCodeInput)
 {
     ui->setupUi(this);
+    _frame = 0;
+    _tcType = PhTimeCodeType25;
+
+    connect(ui->input, SIGNAL(textEdited(QString)), this, SLOT(isTimeCode(QString)));
+
 }
 
 TimeCodeInput::~TimeCodeInput()
 {
     delete ui;
+}
+
+void TimeCodeInput::setTCType(PhTimeCodeType tcType)
+{
+    _tcType = tcType;
+}
+
+void TimeCodeInput::isTimeCode(const QString &text)
+{
+    _frame = PhTimeCode::frameFromString(text, _tcType);
+
+
+
+    emit frameChanged(_frame);
 }
