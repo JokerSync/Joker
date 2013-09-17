ccb62f0300093023068b06402dafe017d6e2f3f7
Martin Delille <martindelille@MacBook-Pro-de-Martin.local>
2013-09-17 20:10:40 +0200

    PhMediaPanel can handle clock

--- tests/TimecodePlayer/mainwindow.cpp
+++ tests/TimecodePlayer/mainwindow.cpp
@@ -10,27 +10,13 @@ MainWindow::MainWindow(QWidget *parent) :
 
 	_clock->setTCType(PhTimeCodeType25);
 	ui->mediaController->setMediaLength(7500);
-	ui->mediaController->setTCType(_clock->getTCType());
+	ui->mediaController->setTCType(_clock->timeCodeType());
 	ui->mediaController->setFirstFrame(_clock->frame());
-
-	connect(ui->mediaController, SIGNAL(playButtonSignal()), this, SLOT(pushPlayButton()));
-	connect(ui->mediaController, SIGNAL(forwardButtonSignal()), this, SLOT(pushForwardButton()));
-	connect(ui->mediaController, SIGNAL(rewindButtonSignal()), this, SLOT(pushRewindButton()));
-	connect(ui->mediaController, SIGNAL(backButtonSignal()), this, SLOT(pushBackButton()));
-	connect(ui->mediaController, SIGNAL(nextFrameButtonSignal()), this, SLOT(pushNextFrameButton()));
-	connect(ui->mediaController, SIGNAL(previousFrameButtonSignal()), this, SLOT(pushPreviousFrameButton()));
-	connect(ui->mediaController, SIGNAL(useSliderCursorSignal(int)), this, SLOT(useSliderCursor(int)));
-	connect(ui->mediaController, SIGNAL(useComboBoxSignal(int)), this, SLOT(selectRate(int)));
-
-	connect(_clock, SIGNAL(frameChanged(PhFrame, PhTimeCodeType)), ui->mediaController, SLOT(onFrameChanged(PhFrame, PhTimeCodeType)));
-	connect(_clock, SIGNAL(rateChanged(PhRate)), ui->mediaController, SLOT(onRateChanged(PhRate)));
-
-	connect(ui->mediaController, SIGNAL(endOfMediaSignal()), this, SLOT(backToBeginning()));
-
+	ui->mediaController->setClock(_clock);
 
 	_timer = new QTimer();
 	connect(_timer, SIGNAL(timeout()), this, SLOT(updateFrame()));
-	_timer->start(1000/PhTimeCode::getFps(_clock->getTCType()));
+	_timer->start(1000/PhTimeCode::getFps(_clock->timeCodeType()));
 }
 
 
@@ -41,80 +27,5 @@ MainWindow::~MainWindow()
 
 void MainWindow::updateFrame()
 {
-	_clock->tick(PhTimeCode::getFps(_clock->getTCType()));
+	_clock->tick(PhTimeCode::getFps(_clock->timeCodeType()));
 }
-
-
-void MainWindow::pushPlayButton()
-{
-	if(_clock->rate() == 0)//If state = pause
-		_clock->setRate(1);
-	else //If state = play
-		_clock->setRate(0);
-}
-
-
-void MainWindow::pushForwardButton()
-{
-	_clock->setRate(3);
-}
-
-
-void MainWindow::pushRewindButton()
-{
-	_clock->setRate(-3);
-}
-
-
-void MainWindow::pushBackButton()
-{
-	_clock->setRate(0);
-	_clock->setFrame(0);
-}
-
-
-void MainWindow::pushNextFrameButton()
-{
-	PhFrame f = _clock->frame() + 1;
-	_clock->setFrame(f);
-}
-
-
-void MainWindow::pushPreviousFrameButton()
-{
-	PhFrame f = _clock->frame() - 1;
-	_clock->setFrame(f);
-}
-
-
-void MainWindow::useSliderCursor(int position)
-{
-	_clock->setFrame(position);
-}
-
-
-void MainWindow::selectRate(int index)
-{
-	switch(index)
-	{
-	case 0:
-		_clock->setTCType(PhTimeCodeType2398);
-		break;
-	case 1:
-		_clock->setTCType(PhTimeCodeType24);
-		break;
-	case 2:
-		_clock->setTCType(PhTimeCodeType25);
-		break;
-	case 3:
-		_clock->setTCType(PhTimeCodeType2997);
-		break;
-	}
-}
-
-void MainWindow::backToBeginning()
-{
-	_clock->setRate(0);
-	_clock->setFrame(ui->mediaController->getMediaLength());
-}
-
--- tests/TimecodePlayer/mainwindow.h
+++ tests/TimecodePlayer/mainwindow.h
@@ -24,37 +24,6 @@ public slots:
 
 	void updateFrame();
 
-	/**
-	 * @brief changeValuePlayButton
-	 * Change the state of _playButtonState when cliking on _playButton
-	 */
-	void pushPlayButton();
-
-	/**
-	 * @brief changeStateFastForwardButton
-	 * Change the state of _fastForwardButtonState when clicking on _fastForwardButton
-	 */
-	void pushForwardButton();
-
-	/**
-	 * @brief changeStateFastRewindButton
-	 * Change the state of _fastForwardButtonState when clicking on _fastForwardButton
-	 */
-	void pushRewindButton();
-
-	void pushBackButton();
-
-	void pushNextFrameButton();
-
-	void pushPreviousFrameButton();
-
-	void useSliderCursor(int position);
-
-	void selectRate(int index);
-
-	void backToBeginning();
-
-
 private:
 
 	Ui::MainWindow *ui;
