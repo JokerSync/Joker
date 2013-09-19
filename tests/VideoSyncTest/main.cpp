fb8633e3ba6695efece1df11ed5bf7a8273f823b
Martin Delille <martindelille@MacBook-Pro-de-Martin.local>
2013-09-19 21:12:21 +0200

    first version of VideoSyncTest using QTVideo

---
+++ tests/VideoSyncTest/main.cpp
@@ -0,0 +1,17 @@
+#include <QtWidgets/QApplication>
+#include "MainView.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    MainView mainView;
+	mainView.resize(800, 600);
+
+    if(argc>1)
+        mainView.openFile(QString(argv[1]));
+
+    mainView.show();
+
+    return app.exec();
+}
