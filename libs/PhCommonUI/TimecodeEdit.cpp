062be086ecd8ea999b72cae973aba9d272a290dd
julienravit <julienravit@Mac-mini-de-PlayR.local>
2013-09-20 16:31:42 +0200

    creation of isTimeCode method

--- libs/PhCommonUI/TimecodeEdit.cpp
+++ libs/PhCommonUI/TimecodeEdit.cpp
@@ -26,5 +26,10 @@ void TimecodeEdit::onTextChanged(QString text)
     _frame = PhTimeCode::frameFromString(text, _tcType);
     //PhTimeCode::ComputeHhMmSsFf(hhmmssff, _frame, _tcType);
 
-    emit frameChanged(_frame, _tcType);
+	emit frameChanged(_frame, _tcType);
+}
+
+void TimecodeEdit::isTimeCode(QString text)
+{
+
 }
