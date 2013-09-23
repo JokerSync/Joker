91b103ac0e726940cc25d78136eaf640eae075c5
julienravit <julienravit@Mac-mini-de-PlayR.local>
2013-09-23 15:59:39 +0200

    Implementation of isTimeCode(QString)

--- libs/PhCommonUI/TimecodeEdit.cpp
+++ libs/PhCommonUI/TimecodeEdit.cpp
@@ -6,7 +6,7 @@ TimecodeEdit::TimecodeEdit(QWidget *parent) :
     _tcType(PhTimeCodeType25)
 {
 
-    connect(this, SIGNAL(textChanged(QString)), this, SLOT(onTextChanged(QString)));
+	connect(this, SIGNAL(textChanged(QString)), this, SLOT(onTextChanged(QString)));
 
 }
 
@@ -22,14 +22,25 @@ void TimecodeEdit::setTCType(PhTimeCodeType tcType)
 
 bool TimecodeEdit::isTimeCode(QString timecode)
 {
+	PhFrame frame;
+	QString text;
+	frame = PhTimeCode::frameFromString(timecode,_tcType);
+	text = PhTimeCode::stringFromFrame(frame, _tcType);
 
+	qDebug() << text <<"/"<< timecode;
+
+	if(text == timecode)
+		return true;
+	else
+		return false;
 }
 
 void TimecodeEdit::onTextChanged(QString text)
 {
-    _frame = PhTimeCode::frameFromString(text, _tcType);
-    //PhTimeCode::ComputeHhMmSsFf(hhmmssff, _frame, _tcType);
-
-	emit frameChanged(_frame, _tcType);
+	if(isTimeCode(text))
+	{
+		_frame = PhTimeCode::frameFromString(text, _tcType);
+		emit frameChanged(_frame, _tcType);
+	}
 }
 
