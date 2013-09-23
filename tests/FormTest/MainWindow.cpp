85b63c1b670eb673bf26b84b79134ef611b450c5
julienravit <julienravit@Mac-mini-de-PlayR.local>
2013-09-23 11:57:45 +0200

    FormTest ++

--- tests/FormTest/MainWindow.cpp
+++ tests/FormTest/MainWindow.cpp
@@ -3,8 +3,7 @@
 
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
-#include "GoTo.h"
-#include "AboutMenu.h"
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -15,7 +14,7 @@ MainWindow::MainWindow(QWidget *parent) :
     _tcType = PhTimeCodeType25;
 	_goto = new PhTimeCodeDialog(this);
 	_about = new AboutMenu(this);
-    ui->timeCodeLabel->setText(PhTimeCode::stringFromFrame(_frame, _tcType));
+	ui->_timeCodeLabel->setText(PhTimeCode::stringFromFrame(_frame, _tcType));
 
     connect(_goto, SIGNAL(frameChanged(PhFrame, PhTimeCodeType)), this, SLOT(updateText(PhFrame, PhTimeCodeType)));
 	loadSettings();
@@ -29,11 +28,12 @@ MainWindow::~MainWindow()
 
 void MainWindow::updateText(PhFrame frame, PhTimeCodeType tcType)
 {
-    ui->timeCodeLabel->setText(PhTimeCode::stringFromFrame(frame, tcType));
+	ui->_timeCodeLabel->setText(PhTimeCode::stringFromFrame(frame, tcType));
 }
 
 void MainWindow::on_actionGoto_triggered()
 {
+	_goto->setFrame(ui->_timeCodeLabel->text());
     _goto->show();
 }
 
@@ -61,7 +61,7 @@ void MainWindow::on__loadButton_clicked()
 void MainWindow::saveSettings()
 {
 	QSettings setting("Organisation","FormTest");
-	setting.setValue("LineEditContent",ui->lineEdit->text());
+	setting.setValue("LineEditContent",ui->_lineEdit->text());
 
 	qDebug() << "Settings saved";
 }
@@ -69,7 +69,7 @@ void MainWindow::saveSettings()
 void MainWindow::loadSettings()
 {
 	QSettings setting("Organisation","FormTest");
-	ui->lineEdit->setText(setting.value("LineEditContent").toString());
+	ui->_lineEdit->setText(setting.value("LineEditContent").toString());
 
 	qDebug() << "Settings loaded";
 }
