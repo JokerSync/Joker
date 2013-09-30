ca9ac4d408aa02847eb32750f4e204317f08c7e8
ThomasAy <thomas@dubware.net>
2013-09-30 16:29:42 +0200

    changing the clock from a pointer to an instance

--- tests/VideoTest/MainView.cpp
+++ tests/VideoTest/MainView.cpp
@@ -7,15 +7,17 @@
 #include <QFileDialog>
 
 #include "PhCommonUI/PhTimeCodeDlg.h"
+#include "PhVideoSynchronizer.h"
 
 MainView::MainView()
 	: QMainWindow(0),
-	  ui(new Ui::MainView)
-{
-
+      ui(new Ui::MainView), _clock(PhTimeCodeType25)
+{   
 	ui->setupUi(this);
-	_clock = ui->_videoView->getClock();
-	ui->mediaController->setClock(_clock);
+    ui->mediaController->setClock(&_clock);
+
+    _VideoSynchronizer.setVideoClock(ui->_videoView->getClock());
+    _VideoSynchronizer.setInternalClock(&_clock);
 
 	connect(ui->actionOpen, SIGNAL(triggered()), this, SLOT(onOpenFile()));
 	connect(&timer, SIGNAL(timeout()), ui->_videoView, SLOT(checkVideoPosition()));
@@ -40,7 +42,7 @@ bool MainView::openFile(QString fileName)
 #warning TODO read first frame from video file
 		ui->mediaController->setFirstFrame(0);
 
-		_clock->setRate(0.0);
+        _clock.setRate(0.0);
 		return true;
     }
 	return false;
@@ -57,31 +59,31 @@ void MainView::onOpenFile()
 
 void MainView::on_actionPlay_pause_triggered()
 {
-	if(_clock->rate()!=0)
-		_clock->setRate(0);
+    if(_clock.rate()!=0)
+        _clock.setRate(0);
 	else
-		_clock->setRate(1);
+        _clock.setRate(1);
 }
 
 void MainView::on_actionNext_frame_triggered()
 {
-    _clock->setFrame(_clock->frame() + 1);
+    _clock.setFrame(_clock.frame() + 1);
 }
 
 void MainView::on_actionPrevious_frame_triggered()
 {
-	_clock->setFrame(_clock->frame() - 1);
+    _clock.setFrame(_clock.frame() - 1);
 }
 
 void MainView::on_actionSet_timestamp_triggered()
 {
-	PhTimeCodeDlg dlg(_clock->timeCodeType(), _clock->frame());
+    PhTimeCodeDlg dlg(_clock.timeCodeType(), _clock.frame());
 	if(dlg.exec() == QDialog::Accepted)
 	{
 		PhFrame frameStamp = ui->_videoView->getFrameStamp();
-		frameStamp += dlg.frame() - _clock->frame();
+        frameStamp += dlg.frame() - _clock.frame();
 		ui->_videoView->setFrameStamp(frameStamp);
 		ui->mediaController->setFirstFrame(frameStamp);
-		_clock->setFrame(dlg.frame());
+        _clock.setFrame(dlg.frame());
 	}
 }
