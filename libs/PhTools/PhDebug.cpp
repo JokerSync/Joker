a5859f5d0e33e3568a15ab45fe72a919115e8332
ThomasAy <thomas@dubware.net>
2013-09-30 17:47:41 +0200

    adding line and filename in the debug function

--- libs/PhTools/PhDebug.cpp
+++ libs/PhTools/PhDebug.cpp
@@ -5,6 +5,8 @@
 
 #include "PhDebug.h"
 
+#include <QStringList>
+
 // Global static pointer used to ensure a single instance of the class.
 PhDebug* PhDebug::d = NULL;
 // This function is called to create an instance of the class.
@@ -15,14 +17,14 @@ PhDebug* PhDebug::d = NULL;
 PhDebug PhDebug::instance()
 {
     if (!d)   // Only allow one instance of class to be generated.
-        d = new PhDebug(true, true, true);
+        d = new PhDebug(true, true, true, true, true);
     return * d;
 }
 
-PhDebug PhDebug::init(bool DispDate, bool DispTime, bool DispFuncName)
+PhDebug PhDebug::init(bool DispDate, bool DispTime, bool DispFuncName, bool DispFileName, bool DispLine)
 {
     if (!d)   // Only allow one instance of class to be generated.
-        d = new PhDebug(DispDate, DispTime, DispFuncName);
+        d = new PhDebug(DispDate, DispTime, DispFuncName, DispFileName, DispLine);
     return * d;
 }
 
@@ -35,30 +37,69 @@ QDebug PhDebug::operator<<(QDebug dbg)
     if (_dispDate)
 		s = QDate::currentDate().toString("dd/MM/yyyy");
 
+    if (_dispDate && _dispTime)
+        s += " ";
+
     // Display timestamp
     if (_dispTime)
         s += QTime::currentTime().toString("hh:mm:ss.zzz");
 
-    dbg << Q(s);
+    dbg << Q(s + ":");
 
     return dbg;
 
 }
 
-PhDebug::PhDebug(bool DispDate, bool DispTime, bool DispFuncName)
+PhDebug::PhDebug(bool DispDate, bool DispTime, bool DispFuncName, bool DispFileName, bool DispLine)
 {
-
+    //qInstallMessageHandler(errorHandler);
     if (!d){
         _dispDate = DispDate;
         _dispTime = DispTime;
         _dispFuncName = DispFuncName;
+        _dispFileName = DispFileName;
+        _dispLine = DispLine;
     }
 }
 
 QString PhDebug::getFuncName(QString name)
 {
     if(PhDebug::instance()._dispFuncName)
-        return name + ":";
+        return "\"" + name + "\"";
     else
-        return ":";
+        return "";
+}
+
+QString PhDebug::getFileName(QString name)
+{
+    if (PhDebug::instance()._dispFileName)
+        return "in " + name.split("/").last();
+    return "";
+}
+
+QString PhDebug::getLine(int line)
+{
+    if (PhDebug::instance()._dispLine)
+        return "@L" + QString::number(line);
+    return "";
+}
+
+
+//Unused at this moment.
+void errorHandler(QtMsgType type, const char *msg)
+{
+    switch (type) {
+        case QtDebugMsg:
+            fprintf(stderr, "%s\n", msg);
+            break;
+        case QtWarningMsg:
+            fprintf(stderr, "\033[1;33mWarning\033[0m: %s\n", msg);
+            break;
+        case QtCriticalMsg:
+            fprintf(stderr, "\033[31mCritical\033[0m: %s\n", msg);
+            break;
+        case QtFatalMsg:
+            fprintf(stderr, "\033[31mFatal\033[0m: %s\n", msg);
+            abort();
+    }
 }
