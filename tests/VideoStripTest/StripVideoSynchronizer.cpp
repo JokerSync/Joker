663ba50fd29296665c39018dec5f6e531d76a574
Martin Delille <martindelille@MacBook-Pro-de-Martin.local>
2013-10-07 00:39:32 +0200

    move VideoStripSynchronizer to VideoStripTest

--- tests/VideoStripTest/StripVideoSynchronizer.cpp
+++ tests/VideoStripTest/StripVideoSynchronizer.cpp
@@ -1,42 +1,42 @@
-#include "PhStripVideoSynchronizer.h"
 #include "PhTools/PhDebug.h"
+#include "StripVideoSynchronizer.h"
 
-PhStripVideoSynchronizer::PhStripVideoSynchronizer() : _stripClock(NULL), _videoClock(NULL)
+StripVideoSynchronizer::StripVideoSynchronizer() : _stripClock(NULL), _videoClock(NULL)
 {
 }
 
-void PhStripVideoSynchronizer::setStripClock(PhClock *clock)
+void StripVideoSynchronizer::setStripClock(PhClock *clock)
 {
 	_stripClock = clock;
 	connect(_stripClock, SIGNAL(frameChanged(PhFrame,PhTimeCodeType)), this, SLOT(onStripFrameChanged(PhFrame,PhTimeCodeType)));
 	connect(_stripClock, SIGNAL(rateChanged(PhRate)), this, SLOT(onStripRateChanged(PhRate)));
 }
 
-void PhStripVideoSynchronizer::setVideoClock(PhClock *clock)
+void StripVideoSynchronizer::setVideoClock(PhClock *clock)
 {
 	_videoClock = clock;
 	connect(_videoClock, SIGNAL(frameChanged(PhFrame,PhTimeCodeType)), this, SLOT(onVideoFrameChanged(PhFrame,PhTimeCodeType)));
 	connect(_videoClock, SIGNAL(rateChanged(PhRate)), this, SLOT(onVideoRateChanged(PhRate)));
 }
 
-void PhStripVideoSynchronizer::onStripFrameChanged(PhFrame frame, PhTimeCodeType tcType)
+void StripVideoSynchronizer::onStripFrameChanged(PhFrame frame, PhTimeCodeType tcType)
 {
 	_videoClock->setFrame(frame);
 }
 
-void PhStripVideoSynchronizer::onStripRateChanged(PhRate rate)
+void StripVideoSynchronizer::onStripRateChanged(PhRate rate)
 {
 	//_videoClock->setRate(rate);
 }
 
-void PhStripVideoSynchronizer::onVideoFrameChanged(PhFrame frame, PhTimeCodeType tcType)
+void StripVideoSynchronizer::onVideoFrameChanged(PhFrame frame, PhTimeCodeType tcType)
 {
 	if(_stripClock->frame() != frame)
 		PHDEBUG << "error :" << _stripClock->frame() << frame;
 #warning TODO handle frame difference error
 }
 
-void PhStripVideoSynchronizer::onVideoRateChanged(PhRate rate)
+void StripVideoSynchronizer::onVideoRateChanged(PhRate rate)
 {
 	_stripClock->setRate(rate);
 }
