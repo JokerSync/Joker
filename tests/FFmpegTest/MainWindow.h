51a19f2f5b59b244ecc4ed1fd2e02b48a24bb620
Martin Delille <martindelille@MacBook-Pro-de-Martin.local>
2013-10-06 17:47:48 +0200

    clean FFmpegTest

--- tests/FFmpegTest/MainWindow.h
+++ tests/FFmpegTest/MainWindow.h
@@ -2,7 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QtAV/QtAV.h>
 
 namespace Ui {
 class MainWindow;
@@ -29,9 +28,6 @@ private slots:
 
 private:
 	Ui::MainWindow *ui;
-
-    QtAV::AVPlayer *mpPlayer;
-    QtAV::VideoRenderer *mpRenderer;
 };
 
 #endif // MAINWINDOW_H
