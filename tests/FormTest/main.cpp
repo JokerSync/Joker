d166279a4f3c2a32588242f1d918e6690acb3603
Martin Delille <martindelille@MacBook-Pro-de-Martin.local>
2013-10-06 15:07:37 +0200

    yuv to rgb conversion++

--- tests/FormTest/main.cpp
+++ tests/FormTest/main.cpp
@@ -1,8 +1,10 @@
 #include "MainWindow.h"
 #include <QApplication>
+#include "PhTools/PhDebug.h"
 
 int main(int argc, char *argv[])
 {
+	PhDebug::init(false, false, true, false, false);
     QApplication a(argc, argv);
     MainWindow window;
 	window.show();
