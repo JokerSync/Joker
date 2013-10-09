eb2a970885742c8e6707dcbfa84d3b334e188105
Martin Delille <martindelille@MacBook-Pro-de-Martin.local>
2013-10-09 22:02:05 +0200

    generate dialog remember the last value

--- tests/GraphicStripTest/GenerateDialog.cpp
+++ tests/GraphicStripTest/GenerateDialog.cpp
@@ -8,14 +8,18 @@
 #include "PhTools/PhTimeCode.h"
 #include "PhTools/PhDebug.h"
 
-GenerateDialog::GenerateDialog(PhStripDoc * doc, QWidget *parent) :
+GenerateDialog::GenerateDialog(QSettings * settings, PhStripDoc * doc, QWidget *parent) :
 	QDialog(parent),
-	ui(new Ui::GenerateDialog)
+	ui(new Ui::GenerateDialog),
+	_settings(settings)
 {
 	this->_doc = doc;
 	ui->setupUi(this);
-	ui->lineEditText->setText("Per hoc minui studium suum existimans Paulus.");
-	ui->lineEditTimeCode->setText("00:00:00:00");
+	ui->lineEditText->setText(_settings->value("textContent", "Per hoc minui studium suum existimans Paulus.").toString());
+	ui->lineEditTimeCode->setFrame(_settings->value("startFrame", 90000).toInt(), _doc->getTCType());
+	ui->spinBoxNbPeople->setValue(_settings->value("peopleNumber", 0).toInt());
+	ui->spinBoxNbText->setValue(_settings->value("textNumber", 0).toInt());
+	ui->spinBoxNbTrack->setValue(_settings->value("trackNumber", 4).toInt());
 	connect(ui->lineEditText, SIGNAL(textChanged(QString)), this, SLOT(onTextChanged()));
 	connect(ui->spinBoxNbText, SIGNAL(valueChanged(int)), this, SLOT(onTextChanged()));
 	connect(ui->buttonBox, SIGNAL(accepted()), this, SLOT(onAccept()));
@@ -50,6 +54,11 @@ void GenerateDialog::onAccept()
 	int nbText = this->ui->spinBoxNbText->value();
 	int nbTracks = this->ui->spinBoxNbTrack->value();
 	QString text = this->ui->lineEditText->text();
-	int timeCode = PhTimeCode::frameFromString(this->ui->lineEditTimeCode->text(), PhTimeCodeType25);
-	_doc->createDoc(text, nbPeople, nbLoop, nbText, nbTracks, timeCode);
+	int startFrame = this->ui->lineEditTimeCode->frame();
+	_doc->createDoc(text, nbPeople, nbLoop, nbText, nbTracks, startFrame);
+	_settings->setValue("startFrame", startFrame);
+	_settings->setValue("peopleNumber", nbPeople);
+	_settings->setValue("textNumber", nbText);
+	_settings->setValue("trackNumber", nbTracks);
+	_settings->setValue("textContent", text);
 }
--- tests/GraphicStripTest/main.cpp
+++ tests/GraphicStripTest/main.cpp
@@ -6,6 +6,7 @@
 
 #include <QApplication>
 #include <QDebug>
+#include <QSettings>
 
 #include "PhStrip/PhStripDoc.h"
 #include "PhTools/PhDebug.h"
@@ -15,18 +16,31 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	MainWindow w;
+	QSettings settings("Phonations","GraphicStripTest");
+	MainWindow w(&settings);
 
 	PhDebug::init(false, false, true, true, true);
     w.show();
 
 	if (argc > 1)
-	{
-		QString fileName = argv[1];
-		w.openFile(fileName);
-	}
+		w.openFile(argv[1]);
 	else
-		w.createFile(1,3,3,3);
+	{
+		QString lastFile = settings.value("lastFile", "").toString();
+		if(QFile::exists(lastFile))
+			w.openFile(lastFile);
+		else
+		{
+			w.createFile(
+						settings.value("peopleNumber", 0).toInt(),
+						settings.value("loopNumber", 0).toInt(),
+						settings.value("textNumber", 0).toInt(),
+						settings.value("trackNumber", 4).toInt(),
+						settings.value("textContent", "Per hoc minui studium suum existimans Paulus.").toString(),
+						settings.value("startFrame", 90000).toInt()
+						);
+		}
+	}
 
     return a.exec();
 }
