1d716713c27c3f7b2479fd3b5dc85976862f1245
ThomasAy <thomas@dubware.net>
2013-10-15 15:24:23 +0200

    first step to glyph way

--- tests/SDLTest/main.cpp
+++ tests/SDLTest/main.cpp
@@ -21,13 +21,14 @@
 #include <QDebug>
 #include <QDir>
 #include <QApplication>
+#include <QTime>
 
 
 #include "PhTools/PhDebug.h"
 
 //Screen attributes
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 const int SCREEN_BPP = 32;
 
 //The surfaces
@@ -44,82 +45,143 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	QApplication a(argc, argv);
+	QTime t;
 
-	PHDEBUG << "SDL_Init()";
-    //Initialize all SDL subsystems
-    if( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
-        return false;
+	//PHDEBUG << "SDL_Init()";
+	//Initialize all SDL subsystems
+	if( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
+		return false;
 
-	PHDEBUG << "SDL_SetVideoMode";
-    //Set up the screen
+	//PHDEBUG << "SDL_SetVideoMode";
+	//Set up the screen
 
 	SDL_Window * window = SDL_CreateWindow("SDLTest", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_SWSURFACE);
 
 	screen = SDL_GetWindowSurface(window);
 
-	PHDEBUG << "SDL_WM_SetCaption";
+	//PHDEBUG << "SDL_WM_SetCaption";
 
-	PHDEBUG << "current path : " << QApplication::applicationDirPath();
+	//	PHDEBUG << "current path : " << QApplication::applicationDirPath();
 	string ressourcesPath = QApplication::applicationDirPath().toStdString();
-	PHDEBUG << "load_image";
-    // Create a surface from picture:
+	//	PHDEBUG << "load_image";
+	// Create a surface from picture:
 	string lookPath = ressourcesPath + "/look.png";
 	image = IMG_Load( lookPath.c_str());
 
-    if( image == NULL )
-    {
-        return 1;
-    }
+	if( image == NULL )
+	{
+		return 1;
+	}
 
-    // Display the picture:
+	// Display the picture:
 	SDL_Rect imageRect = {0, 0, image->w, image->h};
 
-	SDL_BlitSurface( image, NULL, screen, &imageRect );
+	//SDL_BlitSurface( image, NULL, screen, &imageRect );
 
-    // Initialize TTF :
-    if( TTF_Init() == -1 ) {
-        PHDEBUG << "TTF error.";
-        return 2;
-    }
+	// Initialize TTF :
+	if( TTF_Init() == -1 ) {
+		PHDEBUG << "TTF error.";
+		return 2;
+	}
 
-    //Create a font
+	//Create a font
 	string fontPath = ressourcesPath + "/Bedizen.ttf";
 	TTF_Font *font = TTF_OpenFont(fontPath.c_str(), 100 );
-    if (font == NULL)
-        return 3;
+	if (font == NULL)
+		return 3;
 
-    //Font's color
-    SDL_Color textColor = { 0, 0, 255, 1 };
+	//Font's color
+	SDL_Color color={255,255,255};
+	Uint16 ch;
+#define TEST  2
 
-    // Create a text surface:
-    SDL_Surface *surface = TTF_RenderUTF8_Blended( font, "SDL test OK", textColor );
+#if TEST == 0
+	SDL_Surface *surface = TTF_RenderUTF8_Blended(font, "ABCDEFGHIJKL", textColor );
+	SDL_Rect textRect = {0, 100, surface->w, surface->h};
+	SDL_BlitSurface( surface, NULL, screen, &textRect);
 
-    PHDEBUG << "surface :" << surface->w << "x" << surface->h << "/" << surface->format->BytesPerPixel << "/" << surface->pitch;
+#elif TEST == 1
 
-    // Dump the text surface:
-    //MemoryDump(surface->pixels, surface->pitch, surface->h, surface->format->BytesPerPixel);
+	Uint32 rmask, gmask, bmask, amask;
+	rmask = 0x000000ff;
+	gmask = 0x0000ff00;
+	bmask = 0x00ff0000;
+	amask = 0xff000000;
 
-    // Display the text surface:
-	SDL_Rect textRect = {0, 0, surface->w, surface->h};
-	SDL_BlitSurface( surface, NULL, screen, &textRect );
+
+	ch = 65;
+	SDL_Surface * glyph = SDL_CreateRGBSurface(0,screen->w,screen->h,32,rmask,gmask,bmask,amask);
+	SDL_Surface * car = TTF_RenderGlyph_Blended(font,ch,color);
+
+	SDL_Rect glyphRect = {0, 0, 100, 100};
+	SDL_Rect carRect = {100, 0, 300, 300};
+
+	PHDEBUG << SDL_BlitSurface(car, NULL, glyph, &glyphRect);
+	PHDEBUG << SDL_BlitSurface(glyph, NULL, screen, &glyphRect);
+
+
+#elif TEST == 2
+
+	int offsetX = 0;
+	int offsetY = 0;
+
+	Uint32 rmask, gmask, bmask, amask;
+	rmask = 0x000000ff;
+	gmask = 0x0000ff00;
+	bmask = 0x00ff0000;
+	amask = 0xff000000;
+
+
+	SDL_Surface * glyph = SDL_CreateRGBSurface(0,screen->w,screen->h,32,rmask,gmask,bmask,amask);
+	SDL_Rect glyphRect = {0, 0, screen->w, screen->h};
+	SDL_Rect carRect = {100, 0, 300, 300};
+
+	for(ch = 65; ch < 128; ++ch)
+	{
+		SDL_Surface * car = TTF_RenderGlyph_Blended(font,ch,color);
+		SDL_Rect carRect = {offsetX, offsetY, car->w, car->h};
+		if(SDL_BlitSurface( car, NULL, glyph, &carRect ))
+			PHDEBUG << SDL_GetError();
+		if (offsetX + car->w > screen->w)
+		{
+			offsetX = 0;
+			offsetY += car->h;
+		}
+		else
+			offsetX += car->w;
+
+	}
+	SDL_Rect textRect = {0, 0, glyph->w, glyph->h};
+	PHDEBUG << SDL_BlitSurface(glyph, NULL, screen, &glyphRect);
+
+#elif TEST == 3
+	ch = 66;
+	SDL_Surface * car = TTF_RenderGlyph_Blended(font,ch,color);
+	SDL_Surface * glyph = SDL_ConvertSurface(car, car->format, car->flags);
+	SDL_Rect textRect = {0, 0, car->w, car->h};
+	qDebug() << SDL_BlitSurface(glyph, NULL, screen, &textRect);
+#endif
+
+
+
 
 	//Update the screen
 	if( SDL_UpdateWindowSurface(window))
-        return -1;
+		return -1;
 
-    int ret = a.exec();
+	int ret = a.exec();
 
-    //Free the surface and quit SDL
-    SDL_FreeSurface( image );
-    SDL_FreeSurface(message);
-    SDL_FreeSurface(screen);
+	//Free the surface and quit SDL
+	SDL_FreeSurface( image );
+	SDL_FreeSurface(message);
+	SDL_FreeSurface(screen);
 
-    //Quit SDL
-    TTF_CloseFont( font );
-    TTF_Quit();
-    SDL_Quit();
+	//Quit SDL
+	TTF_CloseFont( font );
+	TTF_Quit();
+	SDL_Quit();
 
-    return ret;
+	return ret;
 }
 
 
