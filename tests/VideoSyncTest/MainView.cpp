4f8fa87bdd0e83fccbbc1848e1a4a7e69c4fb1e5
Martin Delille <martindelille@MacBook-Pro-de-Martin.local>
2013-10-23 23:33:38 +0200

    VideoSyncTest compiles

--- tests/VideoSyncTest/MainView.cpp
+++ tests/VideoSyncTest/MainView.cpp
@@ -10,16 +10,12 @@ MainView::MainView()
 	: QMainWindow(0),
 	  ui(new Ui::MainView)
 {
-	_clock = new PhClock(PhTimeCodeType25);
 	ui->setupUi(this);
-	ui->_videoView->setClock(_clock);
+	ui->_videoView->setEngine(&_videoEngine);
 
-	ui->mediaController->setClock(_clock);
+	ui->mediaController->setClock(_videoEngine.clock());
 
 	connect(ui->actionOpen, SIGNAL(triggered()), this, SLOT(onOpenFile()));
-	connect(&timer, SIGNAL(timeout()), ui->_videoView, SLOT(checkVideoPosition()));
-
-	timer.start(10);
 }
 
 
@@ -30,16 +26,14 @@ MainView::~MainView()
 
 bool MainView::openFile(QString fileName)
 {
-    QFileInfo fileInfo(fileName);
-    if (fileInfo.exists())
-    {
-		ui->_videoView->open(fileName);
+    if (_videoEngine.open(fileName))
+	{
 #warning TODO read media length from video file
 		ui->mediaController->setMediaLength(7500);
 #warning TODO read first frame from video file
 		ui->mediaController->setFirstFrame(0);
 
-		_clock->setRate(0.0);
+		_videoEngine.clock()->setRate(0.0);
 		return true;
     }
 	return false;
