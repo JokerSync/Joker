22eee6f029fde0a96456d01716a29209705c8df1
ThomasAy <thomas@dubware.net>
2013-11-04 17:15:36 +0100

    Adding project

---
+++ TabPanelTest/PrefPanel.h
@@ -0,0 +1,27 @@
+/**
+* Copyright (C) 2012-2013 Phonations
+* License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+*/
+
+#ifndef PREFPANEL_H
+#define PREFPANEL_H
+
+#include <QWidget>
+
+namespace Ui {
+class PrefPanel;
+}
+
+class PrefPanel : public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit PrefPanel(QWidget *parent = 0);
+	~PrefPanel();
+
+private:
+	Ui::PrefPanel *ui;
+};
+
+#endif // PREFPANEL_H
