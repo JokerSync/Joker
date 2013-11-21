ccee4917761a157eb2f27626d6fa5b58f709dacc
Martin Delille <martindelille@macbootcamp.lan>
2013-11-21 18:39:11 +0100

    VideoSyncTest remember last file #19

--- tests/VideoSyncTest/main.cpp
+++ tests/VideoSyncTest/main.cpp
@@ -1,17 +1,28 @@
 #include <QtWidgets/QApplication>
 #include "MainView.h"
+#include <QSettings>
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+	QSettings settings("Phonations", "VideoSyncTest");
+	int logLevel = settings.value("logLevel", 1).toInt();
+	PhDebug::init(false, true, true, true, true, true, logLevel, "VideoSyncTest");
 
-    MainView mainView;
+	QApplication app(argc, argv);
+
+	MainView mainView(&settings);
 	mainView.resize(800, 600);
 
-    if(argc>1)
-        mainView.openFile(QString(argv[1]));
-
     mainView.show();
+
+	QString fileName = "";
+	if (argc > 1)
+		fileName = argv[1];
+	else
+		fileName = settings.value("lastFile").toString();
+
+	if(QFile(fileName).exists())
+		mainView.openFile(fileName);
 
     return app.exec();
 }
