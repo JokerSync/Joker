ceb8f1e63aa1efe90f79a05b75b8db4a606b68f0
Martin Delille <martindelille@macbootcamp.lan>
2013-11-21 17:14:31 +0100

    switch strip and video drawing order for sync purpose
    since strip do the tick it can shift strip clock and video clock frame.

--- tests/VideoStripTest/VideoStripView.cpp
+++ tests/VideoStripTest/VideoStripView.cpp
@@ -15,6 +15,6 @@ void VideoStripView::paint()
 	int videoHeight = this->height() * 3 / 4;
 	int videoWidth = this->height();
 	int videoX = (this->width() - videoWidth) / 2;
-	_videoEngine.drawVideo(videoX, 0, videoWidth, videoHeight);
 	_strip.draw(0, videoHeight, this->width(), this->height() - videoHeight);
+	_videoEngine.drawVideo(videoX, 0, videoWidth, videoHeight);
 }
