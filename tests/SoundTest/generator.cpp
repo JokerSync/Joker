7ae146cdebc7c40b800e3a9b75bcc7f319b2e576
ThomasAy <thomas@phonations.com>
2013-12-13 16:43:24 +0100

    adding project able to generate a sound

---
+++ tests/SoundTest/generator.cpp
@@ -0,0 +1,108 @@
+#include "generator.h"
+
+#include <QAudioDeviceInfo>
+#include <QAudioOutput>
+#include <qmath.h>
+#include <qendian.h>
+
+const int DurationSeconds = 1;
+const int ToneSampleRateHz = 16500;
+const int DataSampleRateHz = 44100;
+const int BufferSize      = 32768;
+
+
+Generator::Generator(const QAudioFormat &format,
+                     qint64 durationUs,
+                     int sampleRate,
+                     QObject *parent)
+    :   QIODevice(parent)
+    ,   m_pos(0)
+{
+    generateData(format, durationUs, sampleRate);
+}
+
+Generator::~Generator()
+{
+
+}
+
+void Generator::start()
+{
+    open(QIODevice::ReadOnly);
+}
+
+void Generator::stop()
+{
+    m_pos = 0;
+    close();
+}
+
+void Generator::generateData(const QAudioFormat &format, qint64 durationUs, int sampleRate)
+{
+    const int channelBytes = format.sampleSize() / 8;
+    const int sampleBytes = format.channelCount() * channelBytes;
+
+    qint64 length = (format.sampleRate() * format.channelCount() * (format.sampleSize() / 8))
+                        * durationUs / 100000;
+
+    Q_ASSERT(length % sampleBytes == 0);
+    Q_UNUSED(sampleBytes) // suppress warning in release builds
+
+    m_buffer.resize(length);
+    unsigned char *ptr = reinterpret_cast<unsigned char *>(m_buffer.data());
+    int sampleIndex = 0;
+
+    while (length) {
+        const qreal x = qSin(2 * M_PI * sampleRate * qreal(sampleIndex % format.sampleRate()) / format.sampleRate());
+        for (int i=0; i<format.channelCount(); ++i) {
+            if (format.sampleSize() == 8 && format.sampleType() == QAudioFormat::UnSignedInt) {
+                const quint8 value = static_cast<quint8>((1.0 + x) / 2 * 255);
+                *reinterpret_cast<quint8*>(ptr) = value;
+            } else if (format.sampleSize() == 8 && format.sampleType() == QAudioFormat::SignedInt) {
+                const qint8 value = static_cast<qint8>(x * 127);
+                *reinterpret_cast<quint8*>(ptr) = value;
+            } else if (format.sampleSize() == 16 && format.sampleType() == QAudioFormat::UnSignedInt) {
+                quint16 value = static_cast<quint16>((1.0 + x) / 2 * 65535);
+                if (format.byteOrder() == QAudioFormat::LittleEndian)
+                    qToLittleEndian<quint16>(value, ptr);
+                else
+                    qToBigEndian<quint16>(value, ptr);
+            } else if (format.sampleSize() == 16 && format.sampleType() == QAudioFormat::SignedInt) {
+                qint16 value = static_cast<qint16>(x * 32767);
+                if (format.byteOrder() == QAudioFormat::LittleEndian)
+                    qToLittleEndian<qint16>(value, ptr);
+                else
+                    qToBigEndian<qint16>(value, ptr);
+            }
+
+            ptr += channelBytes;
+            length -= channelBytes;
+        }
+        ++sampleIndex;
+    }
+}
+
+qint64 Generator::readData(char *data, qint64 len)
+{
+    qint64 total = 0;
+    while (len - total > 0) {
+        const qint64 chunk = qMin((m_buffer.size() - m_pos), len - total);
+        memcpy(data + total, m_buffer.constData() + m_pos, chunk);
+        m_pos = (m_pos + chunk) % m_buffer.size();
+        total += chunk;
+    }
+    return total;
+}
+
+qint64 Generator::writeData(const char *data, qint64 len)
+{
+    Q_UNUSED(data);
+    Q_UNUSED(len);
+
+    return 0;
+}
+
+qint64 Generator::bytesAvailable() const
+{
+    return m_buffer.size() + QIODevice::bytesAvailable();
+}
---
+++ tests/SoundTest/generator.h
@@ -0,0 +1,33 @@
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+
+#include <QAudioOutput>
+#include <QByteArray>
+#include <QIODevice>
+#include <QObject>
+
+
+class Generator : public QIODevice
+{
+    Q_OBJECT
+
+public:
+    Generator(const QAudioFormat &format, qint64 durationUs, int sampleRate, QObject *parent);
+    ~Generator();
+
+    void start();
+    void stop();
+
+    qint64 readData(char *data, qint64 maxlen);
+    qint64 writeData(const char *data, qint64 len);
+    qint64 bytesAvailable() const;
+
+private:
+    void generateData(const QAudioFormat &format, qint64 durationUs, int sampleRate);
+
+private:
+    qint64 m_pos;
+    QByteArray m_buffer;
+};
+#endif // GENERATOR_H
