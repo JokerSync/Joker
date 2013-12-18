07a57711c2e42076713f8b36e8d283a12af09b8e
ThomasAy <thomas@phonations.com>
2013-12-18 16:48:37 +0100

    adding graphicLTC

---
+++ libs/PhSync/PhLtcReader.h
@@ -0,0 +1,41 @@
+#ifndef PHLTCREADER_H
+#define PHLTCREADER_H
+
+#include <QString>
+#include <QAudioInput>
+#include <QObject>
+
+
+
+#include "ltc.h"
+
+#include "PhTools/PhClock.h"
+#include "PhTools/PhTimeCode.h"
+
+class PhLtcReader : public QObject
+{
+
+    Q_OBJECT
+
+public:
+    explicit PhLtcReader(QObject *parent = 0);
+
+    void init(QString _input="");
+    QList<QString> inputList();
+    PhClock * clock();
+
+private slots:
+    void onNotify();
+
+private:
+    PhClock _clock;
+
+    QAudioInput *_input;
+
+    qint64 position;
+    QIODevice * buffer2;
+    LTCDecoder * decoder;
+
+};
+
+#endif // PHLTCREADER_H
