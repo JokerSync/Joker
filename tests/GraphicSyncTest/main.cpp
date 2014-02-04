04a4230d3dcd0af52936935f928d538749fdfe64
ThomasAy <thomas@dubware.net>
2014-02-04 12:18:04 +0100

    fix merge

--- tests/GraphicSyncTest/main.cpp
+++ tests/GraphicSyncTest/main.cpp
@@ -3,14 +3,10 @@
 
 int main(int argc, char *argv[])
 {
-<<<<<<< HEAD
-
 	QSettings settings("Phonations", "GraphicSyncTestWindow");
 	int logLevel = settings.value("logLevel", 1).toInt();
 	PhDebug::setLogMask(logLevel);
 
-=======
->>>>>>> origin/master
 	QApplication a(argc, argv);
 	GraphicSyncTestWindow w;
 	w.show();
