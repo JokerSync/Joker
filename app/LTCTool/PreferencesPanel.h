f6fd85addab6aed7794a5b992beb705e5c09cff0
ThomasAy <thomas@phonations.com>
2014-02-12 16:06:57 +0100

    LTCTool app

--- app/LTCTool/PreferencesPanel.h
+++ app/LTCTool/PreferencesPanel.h
@@ -7,14 +7,36 @@ namespace Ui {
 class PreferencesPanel;
 }
 
+/*!
+ * \brief The PreferencesPanel class
+ *
+ * It allow the user to select the desired input and output.
+ */
 class PreferencesPanel : public QDialog
 {
 	Q_OBJECT
 
 public:
+	/*!
+	 * \brief Preferences Panel
+	 *
+	 * Create a modal QDialog which prompt the user for devices.
+	 *
+	 * \param audioOutput	The current output device.
+	 * \param audioInput	The current input device.
+	 * \param parent		The parent.
+	 */
 	explicit PreferencesPanel(QString audioOutput, QString audioInput, QWidget *parent = 0);
 	~PreferencesPanel();
+	/*!
+	 * \brief selectedAudioOutput
+	 * \return The selected output device
+	 */
 	QString selectedAudioOutput();
+	/*!
+	 * \brief selectedAudioInput
+	 * \return The selected input device
+	 */
 	QString selectedAudioInput();
 
 private:
