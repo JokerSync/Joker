3f60742e07eb4a954b03a30fed6f533c0488ecc0
Martin Delille <martin@phonations.com>
2014-03-28 19:51:47 +0100

    VideoTest timestamp++

--- tests/VideoTest/VideoTestWindow.cpp
+++ tests/VideoTest/VideoTestWindow.cpp
@@ -44,6 +44,18 @@ bool VideoTestWindow::openDocument(QString fileName)
 	return true;
 }
 
+void VideoTestWindow::processArg(int argc, char *argv[])
+{
+	PhDocumentWindow::processArg(argc, argv);
+	for(int i = 1; i < argc; i++) {
+		PhFrame firstFrame = PhTimeCode::frameFromString(argv[i], _videoEngine.clock()->timeCodeType());
+		if(firstFrame) {
+			_videoEngine.setFirstFrame(firstFrame);
+			_videoEngine.clock()->setFrame(firstFrame);
+		}
+	}
+}
+
 void VideoTestWindow::resizeEvent(QResizeEvent *)
 {
 	PHDEBUG << this->width() << this->height();
@@ -91,14 +103,30 @@ void VideoTestWindow::on_actionPrevious_frame_triggered()
 
 void VideoTestWindow::on_actionSet_timestamp_triggered()
 {
-	PhTimeCodeDialog dlg(_videoEngine.clock()->timeCodeType(), _videoEngine.clock()->frame());
+	_mediaPanelDialog.hide();
+	PhFrame frame;
+	if(_videoEngine.clock()->frame() < _videoEngine.firstFrame())
+		frame = _videoEngine.firstFrame();
+	else if(_videoEngine.clock()->frame() > _videoEngine.firstFrame() + _videoEngine.length())
+		frame = _videoEngine.lastFrame();
+	else
+		frame = _videoEngine.clock()->frame();
+
+	PhTimeCodeDialog dlg(_videoEngine.clock()->timeCodeType(), frame);
 	if(dlg.exec() == QDialog::Accepted) {
-		PhFrame frameStamp = _videoEngine.firstFrame();
-		frameStamp += dlg.frame() - _videoEngine.clock()->frame();
+		PhFrame frameStamp;
+		if(_videoEngine.clock()->frame() > _videoEngine.firstFrame() + _videoEngine.length())
+			frameStamp = dlg.frame() - (_videoEngine.length() - 1);
+		else if (_videoEngine.clock()->frame() < _videoEngine.firstFrame())
+			frameStamp =  dlg.frame();
+		else
+			frameStamp = _videoEngine.firstFrame() + dlg.frame() - _videoEngine.clock()->frame();
+
 		_videoEngine.setFirstFrame(frameStamp);
 		_mediaPanelDialog.setFirstFrame(frameStamp);
 		_videoEngine.clock()->setFrame(dlg.frame());
 	}
+	_mediaPanelDialog.show();
 }
 
 void VideoTestWindow::on_actionOpen_triggered()
