8eb8271ab58623da60aaa9206e739925fa2c71f6
Martin Delille <martin@phonations.com>
2014-04-03 22:36:23 +0200

    simpler LTCTest

--- tests/LTCTest/main.cpp
+++ tests/LTCTest/main.cpp
@@ -1,12 +1,10 @@
-#include <QCoreApplication>
-
 #include <stdio.h>
 #include <math.h>
 #include "ltc.h"
 
 #include "PhTools/PhDebug.h"
 
-#define BUFFER_SIZE (1024)
+#define BUFFER_SIZE (8192)
 
 /**
  * @brief The application main entry point
@@ -16,59 +14,39 @@
  */
 int main(int argc, char *argv[])
 {
-	QCoreApplication a(argc, argv);
-
-	for(int i = 0; i < argc; i++)
-		PHDEBUG << argv[i];
-	PHDEBUG << "\n\n";
-
-	int apv = 1920;
-	ltcsnd_sample_t sound[BUFFER_SIZE];
-	size_t n;
-	long int total;
-	FILE* f;
-	char* filename;
-	LTCDecoder *decoder;
-	LTCFrameExt frame;
-
-	// fIXME
-	if (argc > 1) {
-		filename = argv[1];
-		if (argc > 2) {
-			sscanf(argv[2], "%i", &apv);
-		}
-	}
-	else {
+	if (argc < 2) {
 		printf("Usage: %s <filename> [audio-frames-per-video-frame]\n", argv[0]);
 		return -1;
 	}
-	f = fopen(filename, "r");
+
+	char *fileName = argv[1];
+	FILE *f = fopen(fileName, "r");
 	if (!f) {
-		fprintf(stderr, "error opening '%s'\n", filename);
+		fprintf(stderr, "error opening '%s'\n", fileName);
 		return -1;
 	}
-	fprintf(stderr, "* reading from: %s\n", filename);
-	total = 0;
-	decoder = ltc_decoder_create(apv, 3840);
+	fprintf(stderr, "* reading from: %s\n", fileName);
+	offset = 0;
+	LTCDecoder *decoder = ltc_decoder_create(1920, 3840);
+	ltcsnd_sample_t sound[BUFFER_SIZE];
 
+	long int offset = 0;
+	size_t byteRead = 0;
 	do {
-		//PHDEBUG << "in da do";
-		n = fread(sound, sizeof(ltcsnd_sample_t), BUFFER_SIZE, f);
+		byteRead = fread(sound, 1, BUFFER_SIZE, f);
 
-		//PHDEBUG << n;
-		ltc_decoder_write(decoder, sound, n, total);
-		//PHDEBUG << "2";
+		ltc_decoder_write(decoder, sound, byteRead, offset);
 
+		LTCFrameExt frame;
 		while (ltc_decoder_read(decoder, &frame)) {
 			SMPTETimecode stime;
 			ltc_frame_to_time(&stime, &frame.ltc, 1);
-			//PHDEBUG << stime.years + 1900 << stime.months << stime.days; // << stime.timezone;
-			PHDEBUG << stime.hours << stime.mins << stime.secs << ":" << stime.frame << "\toffStart " << frame.off_start << "\toffEnd" << frame.off_end;
+			PHDEBUG << stime.hours << stime.mins << stime.secs << ":" << stime.frame;
 		}
-		total += n;
-		//PHDEBUG << n;
+		offset += byteRead;
 	}
-	while (n);
+	while (byteRead);
+
 	fclose(f);
 	ltc_decoder_free(decoder);
 
