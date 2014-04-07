acc24a6dac96fbfc50fb0a0d49871ad0fd6b1567
MartinDelille <martin@phonations.com>
2014-04-07 21:47:38 +0200

    add dmg copy to github page mechanism

---
+++ scripts/update-gh-pages.sh
@@ -0,0 +1,25 @@
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
+
+  # Just list the home folder to check the DMG is here
+  ls -l $HOME/
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Phonations/TravisTest.git  gh-pages
+
+  #go into directory and copy data we're interested in to that directory
+  cd gh-pages
+  cp -Rf $HOME/Joker_v* .
+
+  #add, commit and push files
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages
+
+  echo -e "Done magic\n"
+fi
