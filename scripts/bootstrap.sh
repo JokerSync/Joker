821297b2141c6e91d4b1e41ec10dea31bde86f97
Martin Delille <martin@phonations.com>
2014-04-13 11:10:07 +0200

    fix macos sdl installation

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,7 +5,7 @@ echo "Mac OS X detected"
 
 brew update
 brew install ffmpeg libav portaudio 
-brew install doxygen uncrustify node npm pyenv
+brew install doxygen uncrustify node npm pyenv wget
 eval "$(pyenv init -)"
 pyenv install 2.7.2
 pyenv global 2.7.2
