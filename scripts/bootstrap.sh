f63baf452a5be30eae505003934677b3bb0ad0c0
Martin Delille <martin@phonations.com>
2014-04-19 15:53:33 +0200

    reenable AutoTest build

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,14 +4,14 @@ if [ "$(uname)" == "Darwin" ]; then
 echo "Mac OS X detected"
 
 brew update
-brew install ffmpeg libav portaudio 
+brew install ffmpeg libav portaudio
 brew install python doxygen uncrustify node npm pyenv wget
-eval "$(pyenv init -)"
-pyenv install 2.7.2
-pyenv global 2.7.2
-pyenv rehash
-pip install cpp-coveralls
-pyenv rehash
+#eval "$(pyenv init -)"
+#pyenv install 2.7.2
+#pyenv global 2.7.2
+#pyenv rehash
+#pip install cpp-coveralls
+#pyenv rehash
 
 brew install qt5
 find /usr/local/Cellar/ -perm -ugo+r -iname "*dylib*" -exec chmod +w {} \;
@@ -32,4 +32,3 @@ sudo apt-get install doxygen uncrustify
 
 sudo pip install cpp-coveralls
 fi
-
