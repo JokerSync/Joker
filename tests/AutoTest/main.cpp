a340196b4b0573fb1e65a24e8cc12891148911f4
Martin Delille <martin@phonations.com>
2014-05-04 15:52:35 +0200

    travis only ascii art result

--- tests/AutoTest/main.cpp
+++ tests/AutoTest/main.cpp
@@ -26,9 +26,8 @@ int main(int argc, char *argv[])
 	bool testUi = testAll;
 	bool testGraphicStrip = testAll;
 	bool testGraphicText = testAll;
-	bool quiet = false;
 
-	bool success = true;
+	int result = 0;
 
 	for(int i = 1; i < argc; i++) {
 		if(strcmp(argv[i], "tc") == 0)
@@ -45,170 +44,167 @@ int main(int argc, char *argv[])
 			testGraphicStrip = true;
 		else if(strcasecmp(argv[i], "graphictext") == 0)
 			testGraphicText = true;
-		else if(strcmp(argv[i], "quiet") == 0)
-			quiet = true;
 	}
 
 	if(testTC) {
 		// Testing PhTimeCode
 		TimeCodeTest tcTest;
-		success &= !QTest::qExec(&tcTest);
+		result += QTest::qExec(&tcTest);
 	}
 
 	if(testSettings) {
 		// Testing PhSettings
 		SettingsTest settingsTest;
-		success &= !QTest::qExec(&settingsTest);
+		result += QTest::qExec(&settingsTest);
 	}
 
 	if(testDoc) {
 		// Testing PhStripDoc
 		StripDocTest docTest;
-		success &= !QTest::qExec(&docTest);
+		result += QTest::qExec(&docTest);
 	}
 
 	if(testSony) {
 		// Testing PhSonyController
 		SonyControllerTest sonyTest;
-		success &= !QTest::qExec(&sonyTest);
+		result += QTest::qExec(&sonyTest);
 	}
 
 	if(testUi) {
 		// Testing PhTimeCodeEdit
 		TimeCodeEditTest tcEditTest;
-		success &= !QTest::qExec(&tcEditTest);
+		result += QTest::qExec(&tcEditTest);
 
 		LockableSpinBoxTest spinBoxTest;
-		success &= !QTest::qExec(&spinBoxTest);
+		result += QTest::qExec(&spinBoxTest);
 
 		WindowTest windowTest;
-		success &= !QTest::qExec(&windowTest);
+		result += QTest::qExec(&windowTest);
 	}
 
 	if(testGraphicStrip) {
 		GraphicStripTest viewTest;
-		success &= !QTest::qExec(&viewTest);
+		result += QTest::qExec(&viewTest);
 	}
 	if(testGraphicText) {
 		GraphicTextTest viewTest;
-		success &= !QTest::qExec(&viewTest);
+		result += QTest::qExec(&viewTest);
 	}
 
 	QThread::msleep(500);
 
-	if(quiet)
-		return !success;
+	if(qgetenv("TRAVIS") == "true") {
 
-	if(success) {
+		if(result == 0) {
 
-		PHDEBUG << "                            ************";
-		PHDEBUG << "                         *****************";
-		PHDEBUG << "                       ********************";
-		PHDEBUG << "                        ********************";
-		PHDEBUG << "                         ********";
-		PHDEBUG << "                          ********                 *********";
-		PHDEBUG << "                           ***********              *********";
-		PHDEBUG << "              *********      ****       *            *********";
-		PHDEBUG << "            *            ***               *          *********";
-		PHDEBUG << "          *                       **         *          ********";
-		PHDEBUG << "         *                         **         *       ***********";
-		PHDEBUG << "        *        *****              **      *********************";
-		PHDEBUG << "       *        *******                     *********************";
-		PHDEBUG << "       *         *****                         *****************";
-		PHDEBUG << "       *                                        *";
-		PHDEBUG << "        *     *               *                 *";
-		PHDEBUG << "         *     *             *                  *";
-		PHDEBUG << "           *    *           *                  *";
-		PHDEBUG << "             *    *       *                   *";
-		PHDEBUG << "                *   *****                   *";
-		PHDEBUG << "       ***               *     ************";
-		PHDEBUG << "      *   *            *      *";
-		PHDEBUG << "      *   *          *********          ***";
-		PHDEBUG << "  ****    **********        *          *   *";
-		PHDEBUG << " *               *         *           *   *";
-		PHDEBUG << "*              *           *************    ****";
-		PHDEBUG << "*      *******                                  *";
-		PHDEBUG << "*      *    *                                    *";
-		PHDEBUG << "  *****    *              *****************      *";
-		PHDEBUG << "          *               *               *      *";
-		PHDEBUG << "         *      ****      *                *****";
-		PHDEBUG << "         *    *      **   *";
-		PHDEBUG << "         *   *         ** *";
-		PHDEBUG << "      *** *  *            *";
-		PHDEBUG << "     *    *   *            *    *";
-		PHDEBUG << "      *    *   *           *   * *";
-		PHDEBUG << "       *     *  *          *  *  *";
-		PHDEBUG << "         *     * *         *  *  *";
-		PHDEBUG << "          *       *        *  *  *";
-		PHDEBUG << "            *       *    *****   *";
-		PHDEBUG << "               *      * * *****  *";
-		return 0;
+			PHDEBUG << "                            ************";
+			PHDEBUG << "                         *****************";
+			PHDEBUG << "                       ********************";
+			PHDEBUG << "                        ********************";
+			PHDEBUG << "                         ********";
+			PHDEBUG << "                          ********                 *********";
+			PHDEBUG << "                           ***********              *********";
+			PHDEBUG << "              *********      ****       *            *********";
+			PHDEBUG << "            *            ***               *          *********";
+			PHDEBUG << "          *                       **         *          ********";
+			PHDEBUG << "         *                         **         *       ***********";
+			PHDEBUG << "        *        *****              **      *********************";
+			PHDEBUG << "       *        *******                     *********************";
+			PHDEBUG << "       *         *****                         *****************";
+			PHDEBUG << "       *                                        *";
+			PHDEBUG << "        *     *               *                 *";
+			PHDEBUG << "         *     *             *                  *";
+			PHDEBUG << "           *    *           *                  *";
+			PHDEBUG << "             *    *       *                   *";
+			PHDEBUG << "                *   *****                   *";
+			PHDEBUG << "       ***               *     ************";
+			PHDEBUG << "      *   *            *      *";
+			PHDEBUG << "      *   *          *********          ***";
+			PHDEBUG << "  ****    **********        *          *   *";
+			PHDEBUG << " *               *         *           *   *";
+			PHDEBUG << "*              *           *************    ****";
+			PHDEBUG << "*      *******                                  *";
+			PHDEBUG << "*      *    *                                    *";
+			PHDEBUG << "  *****    *              *****************      *";
+			PHDEBUG << "          *               *               *      *";
+			PHDEBUG << "         *      ****      *                *****";
+			PHDEBUG << "         *    *      **   *";
+			PHDEBUG << "         *   *         ** *";
+			PHDEBUG << "      *** *  *            *";
+			PHDEBUG << "     *    *   *            *    *";
+			PHDEBUG << "      *    *   *           *   * *";
+			PHDEBUG << "       *     *  *          *  *  *";
+			PHDEBUG << "         *     * *         *  *  *";
+			PHDEBUG << "          *       *        *  *  *";
+			PHDEBUG << "            *       *    *****   *";
+			PHDEBUG << "               *      * * *****  *";
+		}
+		else {
+			PHDEBUG << "                                     _";
+			PHDEBUG << "                                     gs        ,8+";
+			PHDEBUG << "                                    d88        888.";
+			PHDEBUG << "                                    888        888b";
+			PHDEBUG << "                                   8888        8888";
+			PHDEBUG << "                                   8888        8888";
+			PHDEBUG << "                                   8888        8888.";
+			PHDEBUG << "                                  ,8888        8888";
+			PHDEBUG << "                                  88888        88888";
+			PHDEBUG << "                                  88888        88889";
+			PHDEBUG << "                                  88888        88888";
+			PHDEBUG << "                                  88888        88888";
+			PHDEBUG << "                                  88888        88888";
+			PHDEBUG << "                                  88888        88888";
+			PHDEBUG << "                                  88888        88888";
+			PHDEBUG << "                                  88888        88888";
+			PHDEBUG << "                                  88888        88888";
+			PHDEBUG << "                                  88888        88888";
+			PHDEBUG << "                                  88888        88888";
+			PHDEBUG << "                                  88888        88888";
+			PHDEBUG << "                                  ~8888        8888~";
+			PHDEBUG << "                                   8888Y+y.'fsY8888";
+			PHDEBUG << "                                   Y8888+lif'f+8888";
+			PHDEBUG << "                                   '8886~~~~~~~7888";
+			PHDEBUG << "                                   88`          V88";
+			PHDEBUG << "                                 ,88       _    d88";
+			PHDEBUG << "                                 88   __+  ]]   ~ Yi";
+			PHDEBUG << "                               d88`   !8!  ]]]    !8.";
+			PHDEBUG << "                   _cooooooooood888i   889  ]]]]    88";
+			PHDEBUG << "                i88~~~~` ~Y8~~~Y~808   808   ]]]    88";
+			PHDEBUG << "              g88f`       '~`    808   808   ]]]    888";
+			PHDEBUG << "             d88                 !8!   !8!    ]]    88!";
+			PHDEBUG << "            88!                                     d8";
+			PHDEBUG << "           ,88                                     ,8!";
+			PHDEBUG << "           888     ,d88888.                       ,d8";
+			PHDEBUG << "           888   ,888888888]                     g88";
+			PHDEBUG << "           '88.  !888888888]                  ,g88f`";
+			PHDEBUG << "            !88.                             ,888";
+			PHDEBUG << "             '88nsnnnnnnnnnnnnnn___________nnn88~";
+			PHDEBUG << "              '~~~~~~~~~~~~~~~~~88888i  g8~`";
+			PHDEBUG << "                                88  88` T8`";
+			PHDEBUG << "                                88     i8f";
+			PHDEBUG << "                                88    ,88";
+			PHDEBUG << "                                88    888";
+			PHDEBUG << "                                888b____d88";
+			PHDEBUG << "                                88888888888";
+			PHDEBUG << "                               ~88~~~~~~Y8~";
+			PHDEBUG << "                              ,88`      88";
+			PHDEBUG << "                              d8        88";
+			PHDEBUG << "                              88        '88";
+			PHDEBUG << "                             88          88";
+			PHDEBUG << "                             88          !8b";
+			PHDEBUG << "                             Y8       /\\  ~88";
+			PHDEBUG << "                             !8.      88   ~88s.      o";
+			PHDEBUG << "                              8b      88   88!88b.    #o_";
+			PHDEBUG << "                              88.     88   '888888b    88o";
+			PHDEBUG << "                              8V8     88    88~~V88b   d88s";
+			PHDEBUG << "                   ,____d8____8.8!    88    88   !88   d888";
+			PHDEBUG << "                _o88f~~~  o8888888888g88    88!   8b.  88088";
+			PHDEBUG << "               g88~,_i  ,8888f~~~~~~~V88    8b__gg888888~ 889";
+			PHDEBUG << "              ,88  88   88  ,d!   ,.        8888_.!88d88_8889      ,";
+			PHDEBUG << "_/\\===+/\\~~T/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\";
+			PHDEBUG << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+		}
 	}
-	else {
-		PHDEBUG << "                                     _";
-		PHDEBUG << "                                     gs        ,8+";
-		PHDEBUG << "                                    d88        888.";
-		PHDEBUG << "                                    888        888b";
-		PHDEBUG << "                                   8888        8888";
-		PHDEBUG << "                                   8888        8888";
-		PHDEBUG << "                                   8888        8888.";
-		PHDEBUG << "                                  ,8888        8888";
-		PHDEBUG << "                                  88888        88888";
-		PHDEBUG << "                                  88888        88889";
-		PHDEBUG << "                                  88888        88888";
-		PHDEBUG << "                                  88888        88888";
-		PHDEBUG << "                                  88888        88888";
-		PHDEBUG << "                                  88888        88888";
-		PHDEBUG << "                                  88888        88888";
-		PHDEBUG << "                                  88888        88888";
-		PHDEBUG << "                                  88888        88888";
-		PHDEBUG << "                                  88888        88888";
-		PHDEBUG << "                                  88888        88888";
-		PHDEBUG << "                                  88888        88888";
-		PHDEBUG << "                                  ~8888        8888~";
-		PHDEBUG << "                                   8888Y+y.'fsY8888";
-		PHDEBUG << "                                   Y8888+lif'f+8888";
-		PHDEBUG << "                                   '8886~~~~~~~7888";
-		PHDEBUG << "                                   88`          V88";
-		PHDEBUG << "                                 ,88       _    d88";
-		PHDEBUG << "                                 88   __+  ]]   ~ Yi";
-		PHDEBUG << "                               d88`   !8!  ]]]    !8.";
-		PHDEBUG << "                   _cooooooooood888i   889  ]]]]    88";
-		PHDEBUG << "                i88~~~~` ~Y8~~~Y~808   808   ]]]    88";
-		PHDEBUG << "              g88f`       '~`    808   808   ]]]    888";
-		PHDEBUG << "             d88                 !8!   !8!    ]]    88!";
-		PHDEBUG << "            88!                                     d8";
-		PHDEBUG << "           ,88                                     ,8!";
-		PHDEBUG << "           888     ,d88888.                       ,d8";
-		PHDEBUG << "           888   ,888888888]                     g88";
-		PHDEBUG << "           '88.  !888888888]                  ,g88f`";
-		PHDEBUG << "            !88.                             ,888";
-		PHDEBUG << "             '88nsnnnnnnnnnnnnnn___________nnn88~";
-		PHDEBUG << "              '~~~~~~~~~~~~~~~~~88888i  g8~`";
-		PHDEBUG << "                                88  88` T8`";
-		PHDEBUG << "                                88     i8f";
-		PHDEBUG << "                                88    ,88";
-		PHDEBUG << "                                88    888";
-		PHDEBUG << "                                888b____d88";
-		PHDEBUG << "                                88888888888";
-		PHDEBUG << "                               ~88~~~~~~Y8~";
-		PHDEBUG << "                              ,88`      88";
-		PHDEBUG << "                              d8        88";
-		PHDEBUG << "                              88        '88";
-		PHDEBUG << "                             88          88";
-		PHDEBUG << "                             88          !8b";
-		PHDEBUG << "                             Y8       /\\  ~88";
-		PHDEBUG << "                             !8.      88   ~88s.      o";
-		PHDEBUG << "                              8b      88   88!88b.    #o_";
-		PHDEBUG << "                              88.     88   '888888b    88o";
-		PHDEBUG << "                              8V8     88    88~~V88b   d88s";
-		PHDEBUG << "                   ,____d8____8.8!    88    88   !88   d888";
-		PHDEBUG << "                _o88f~~~  o8888888888g88    88!   8b.  88088";
-		PHDEBUG << "               g88~,_i  ,8888f~~~~~~~V88    8b__gg888888~ 889";
-		PHDEBUG << "              ,88  88   88  ,d!   ,.        8888_.!88d88_8889      ,";
-		PHDEBUG << "_/\\===+/\\~~T/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\";
-		PHDEBUG << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
 
-		return -1;
-	}
+	return result;
 }
