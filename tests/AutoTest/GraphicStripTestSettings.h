f89c443e29b4987ce8003a365ca4f7377716c819
Martin Delille <martin@phonations.com>
2014-05-10 14:38:36 +0200

    uncrustify

--- tests/AutoTest/GraphicStripTestSettings.h
+++ tests/AutoTest/GraphicStripTestSettings.h
@@ -1,7 +1,7 @@
 /**
-* Copyright (C) 2012-2014 Phonations
-* License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
-*/
+ * Copyright (C) 2012-2014 Phonations
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ */
 
 #ifndef GRAPHICSTRIPTESTSETTINGS_H
 #define GRAPHICSTRIPTESTSETTINGS_H
@@ -13,22 +13,50 @@ class GraphicStripTestSettings : public PhGraphicStripSettings
 {
 public:
 	// PhGraphicSettings
-	int screenDelay() { return 0; }
-	bool displayInfo() {return false;}
-	bool resetInfo() {return false;}
+	int screenDelay() {
+		return 0;
+	}
+	bool displayInfo() {
+		return false;
+	}
+	bool resetInfo() {
+		return false;
+	}
 
 	// PhGraphicStripSettings :
-	float stripHeight() { return 1;}
-	int horizontalTimePerPixel() { return 80;}
-	int verticalTimePerPixel() { return 1000;}
-	QString textFontFile() { return QCoreApplication::applicationDirPath() + "/SWENSON.TTF";}
-	int textBoldness() { return 1;}
-	bool stripTestMode() { return false;}
-	bool displayNextText() { return true;}
-	bool invertColor() { return false;}
-	bool displayRuler() { return false;}
-	int rulerTimeIn() { return 0;}
-	int timeBetweenRuler() { return 48000;}
+	float stripHeight() {
+		return 1;
+	}
+	int horizontalTimePerPixel() {
+		return 80;
+	}
+	int verticalTimePerPixel() {
+		return 1000;
+	}
+	QString textFontFile() {
+		return QCoreApplication::applicationDirPath() + "/SWENSON.TTF";
+	}
+	int textBoldness() {
+		return 1;
+	}
+	bool stripTestMode() {
+		return false;
+	}
+	bool displayNextText() {
+		return true;
+	}
+	bool invertColor() {
+		return false;
+	}
+	bool displayRuler() {
+		return false;
+	}
+	int rulerTimeIn() {
+		return 0;
+	}
+	int timeBetweenRuler() {
+		return 48000;
+	}
 };
 
 #endif // GRAPHICSTRIPTESTSETTINGS_H
