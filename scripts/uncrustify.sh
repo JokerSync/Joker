857a45d09c61f1f86cef59a8101e88ce09678cf6
Martin Delille <martin@phonations.com>
2014-05-22 12:37:01 +0200

    uncrustify doesn't parse vendor folder

--- scripts/uncrustify.sh
+++ scripts/uncrustify.sh
@@ -1,7 +1,9 @@
 #!/bin/bash          
 # This script runs Uncrustify
 
-uncrustify -c .uncrustify.cfg */*/*.cpp */*/*.h --replace --no-backup
+uncrustify -c .uncrustify.cfg app/*/*.cpp */*/*.h --replace --no-backup
+uncrustify -c .uncrustify.cfg libs/*/*.cpp */*/*.h --replace --no-backup
+uncrustify -c .uncrustify.cfg tests/*/*.cpp */*/*.h --replace --no-backup
 
 n=$(git diff --dirstat=files | wc -l)
 git status
