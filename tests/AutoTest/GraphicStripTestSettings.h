13bf4f3caf435f76264932291b0abb0fe3b571e8
ThomasAy <thomas@phonations.com>
2014-06-03 11:33:30 +0200

    uncrustify

--- tests/AutoTest/GraphicStripTestSettings.h
+++ tests/AutoTest/GraphicStripTestSettings.h
@@ -1,7 +1,7 @@
 /**
-* Copyright (C) 2012-2014 Phonations
-* License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
-*/
+ * Copyright (C) 2012-2014 Phonations
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ */
 
 #ifndef GRAPHICSTRIPTESTSETTINGS_H
 #define GRAPHICSTRIPTESTSETTINGS_H
