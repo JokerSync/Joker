4fce2967a74c36105533ab014540781846917af1
Martin Delille <martin@phonations.com>
2014-07-17 11:56:31 +0200

    default string list settings value

--- libs/PhTools/PhGenericSettings.cpp
+++ libs/PhTools/PhGenericSettings.cpp
@@ -76,17 +76,21 @@ void PhGenericSettings::setStringList(QString name, QStringList list)
 	_settings.endArray();
 }
 
-QStringList PhGenericSettings::stringList(QString name)
+QStringList PhGenericSettings::stringList(QString name, QStringList defaultValue)
 {
 	QStringList list;
 	int size = _settings.beginReadArray(name);
-	for(int i = 0; i < size; i++) {
-		_settings.setArrayIndex(i);
-		list.append(_settings.value("listItem").toString());
-	}
-	_settings.endArray();
+	if(size == 0)
+		return defaultValue;
+	else {
+		for(int i = 0; i < size; i++) {
+			_settings.setArrayIndex(i);
+			list.append(_settings.value("listItem").toString());
+		}
+		_settings.endArray();
 
-	return list;
+		return list;
+	}
 }
 
 void PhGenericSettings::setByteArray(QString name, QByteArray array)
