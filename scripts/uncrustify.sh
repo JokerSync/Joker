c0013dfe32ac538bf82175ca439befa43006ca8b
Martin Delille <martin@phonations.com>
2014-08-04 18:11:51 +0200

    uncrustify parse only app/libs/tests folder
    it was the case for ".cpp" files but not ".h"

--- scripts/uncrustify.sh
+++ scripts/uncrustify.sh
@@ -1,9 +1,9 @@
 #!/bin/bash          
 # This script runs Uncrustify
 
-uncrustify -c .uncrustify.cfg app/*/*.cpp */*/*.h --replace --no-backup
-uncrustify -c .uncrustify.cfg libs/*/*.cpp */*/*.h --replace --no-backup
-uncrustify -c .uncrustify.cfg tests/*/*.cpp */*/*.h --replace --no-backup
+uncrustify -c .uncrustify.cfg app/*/*.cpp app/*/*.h --replace --no-backup
+uncrustify -c .uncrustify.cfg libs/*/*.cpp libs/*/*.h --replace --no-backup
+uncrustify -c .uncrustify.cfg tests/*/*.cpp tests/*/*.h --replace --no-backup
 
 n=$(git diff --dirstat=files | wc -l)
 git status
