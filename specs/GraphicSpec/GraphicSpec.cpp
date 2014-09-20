47561a9c667f34748b9829fe98fbc683456afac1
Martin Delille <martin@phonations.com>
2014-09-20 13:32:15 +0200

    graphic spec use bandit

--- specs/GraphicSpec/GraphicSpec.cpp
+++ specs/GraphicSpec/GraphicSpec.cpp
@@ -5,7 +5,7 @@
 
 #include <QThread>
 
-#include "igloo/igloo_alt.h"
+#include "bandit/bandit.h"
 
 #include "PhTools/PhDebug.h"
 #include "PhTools/PhPictureTools.h"
@@ -15,112 +15,113 @@
 #include "PhGraphic/PhGraphicTexturedRect.h"
 #include "PhGraphic/PhGraphicImage.h"
 
-using namespace igloo;
+using namespace bandit;
 
-Describe(graphic_test) {
-	static void SetUpContext() {
-		PhDebug::disable();
-	}
-
-	It(call_paint_when_shown) {
-		PhGraphicView view(64, 64);
-
-		bool paintCalled = false;
-
-		QObject::connect(&view, &PhGraphicView::paint, [&](int w, int h) {
-			paintCalled = true;
-			Assert::That(w, Equals(64));
-			Assert::That(h, Equals(64));
+go_bandit([](){
+	describe("graphic_test", [&](){
+		before_each([&](){
+			PhDebug::disable();
 		});
 
-		view.show();
+		it("call_paint_when_shown", [&](){
+			PhGraphicView view(64, 64);
+
+			bool paintCalled = false;
+
+			QObject::connect(&view, &PhGraphicView::paint, [&](int w, int h) {
+				paintCalled = true;
+				Assert::That(w, Equals(64));
+				Assert::That(h, Equals(64));
+			});
+
+			view.show();
 
 #warning /// @todo remove if unnecessary
-		//		QApplication::processEvents();
+			//		QApplication::processEvents();
 
-		Assert::That(paintCalled);
-	}
-
-	It(draw_a_rect) {
-		PhGraphicView view(32, 32);
-
-		PhGraphicSolidRect rect;
-		rect.setColor(Qt::red);
-
-		QObject::connect(&view, &PhGraphicView::paint, [&](int w, int h) {
-			rect.setSize(w / 2, h / 2);
-			rect.draw();
+			Assert::That(paintCalled);
 		});
 
-		view.show();
+		it("draw_a_rect", [&](){
+			PhGraphicView view(32, 32);
+
+			PhGraphicSolidRect rect;
+			rect.setColor(Qt::red);
+
+			QObject::connect(&view, &PhGraphicView::paint, [&](int w, int h) {
+				rect.setSize(w / 2, h / 2);
+				rect.draw();
+			});
+
+			view.show();
 
 #warning /// @todo remove if unnecessary
-		//		QApplication::processEvents();
+			//		QApplication::processEvents();
 
-		// Note: using view.grabFrameBuffer() causes timing issues.
-		// There is no warranty that the scene has been fully rendered.
-		// Use view.renderPixmap().toImage() instead.
-		QImage resultImage(view.renderPixmap(32, 32).toImage());
-		QString resultFile = "rectTest.result.bmp";
-		resultImage.save(resultFile);
-		QString expectedFile = "rectTest.expected.bmp";
-		QImage expectedImage(expectedFile);
+			// Note: using view.grabFrameBuffer() causes timing issues.
+			// There is no warranty that the scene has been fully rendered.
+			// Use view.renderPixmap().toImage() instead.
+			QImage resultImage(view.renderPixmap(32, 32).toImage());
+			QString resultFile = "rectTest.result.bmp";
+			resultImage.save(resultFile);
+			QString expectedFile = "rectTest.expected.bmp";
+			QImage expectedImage(expectedFile);
 
-		unsigned int result = PhPictureTools::compare(resultImage, expectedImage);
-		Assert::That(result, Equals(0));
-	}
-
-	It(draw_an_image) {
-		PhGraphicView view(64, 64);
-
-		PhGraphicImage image("rgbPatternTest.expected.bmp");
-
-		QObject::connect(&view, &PhGraphicView::paint, [&](int w, int h) {
-			image.setSize(w, h);
-			image.draw();
+			unsigned int result = PhPictureTools::compare(resultImage, expectedImage);
+			Assert::That(result, Equals(0));
 		});
 
-		view.show();
+		it("draw_an_image", [&](){
+			PhGraphicView view(64, 64);
 
-		QImage resultImage(view.renderPixmap(64, 64).toImage());
-		QString resultFile = "imageTest.result.bmp";
-		resultImage.save(resultFile);
+			PhGraphicImage image("rgbPatternTest.expected.bmp");
 
-		// The expected result should be the same than the input (rgbPatternTest.expected.bmp)
-		// but it turns out that image is altered when opened with IMG_Load()
-		// (see PhGraphicImage::init())
-		QString expectedFile = "imageTest.expected.bmp";
-		QImage expectedImage(expectedFile);
+			QObject::connect(&view, &PhGraphicView::paint, [&](int w, int h) {
+				image.setSize(w, h);
+				image.draw();
+			});
 
-		unsigned int result = PhPictureTools::compare(resultImage, expectedImage);
-		Assert::That(result, Equals(0));
-	}
+			view.show();
 
-	It(draw_a_rgb_pattern) {
-		int w = 64;
-		int h = 64;
-		PhGraphicView view(w, h);
+			QImage resultImage(view.renderPixmap(64, 64).toImage());
+			QString resultFile = "imageTest.result.bmp";
+			resultImage.save(resultFile);
 
-		PhGraphicTexturedRect rect(0, 0, w, h);
+			// The expected result should be the same than the input (rgbPatternTest.expected.bmp)
+			// but it turns out that image is altered when opened with IMG_Load()
+			// (see PhGraphicImage::init())
+			QString expectedFile = "imageTest.expected.bmp";
+			QImage expectedImage(expectedFile);
 
-		QObject::connect(&view, &PhGraphicView::paint, [&](int w, int h) {
+			unsigned int result = PhPictureTools::compare(resultImage, expectedImage);
+			Assert::That(result, Equals(0));
+		});
+
+		it("draw_a_rgb_pattern", [&](){
+			int w = 64;
+			int h = 64;
+			PhGraphicView view(w, h);
+
+			PhGraphicTexturedRect rect(0, 0, w, h);
+
+			QObject::connect(&view, &PhGraphicView::paint, [&](int w, int h) {
 #warning /// @todo try to make it before (creating the texture on auto init)
-			unsigned char * buffer = PhPictureTools::generateRGBPattern(w, h);
-			rect.createTextureFromRGBBuffer(buffer, w, h);
-			delete buffer;
-			rect.draw();
+				unsigned char * buffer = PhPictureTools::generateRGBPattern(w, h);
+				rect.createTextureFromRGBBuffer(buffer, w, h);
+				delete buffer;
+				rect.draw();
+			});
+
+			view.show();
+
+			QImage resultImage(view.renderPixmap(64, 64).toImage());
+			QString resultFile = "rgbPatternTest.result.bmp";
+			resultImage.save(resultFile);
+			QString expectedFile = "rgbPatternTest.expected.bmp";
+			QImage expectedImage(expectedFile);
+
+			unsigned int result = PhPictureTools::compare(resultImage, expectedImage);
+			Assert::That(result, Equals(0));
 		});
-
-		view.show();
-
-		QImage resultImage(view.renderPixmap(64, 64).toImage());
-		QString resultFile = "rgbPatternTest.result.bmp";
-		resultImage.save(resultFile);
-		QString expectedFile = "rgbPatternTest.expected.bmp";
-		QImage expectedImage(expectedFile);
-
-		unsigned int result = PhPictureTools::compare(resultImage, expectedImage);
-		Assert::That(result, Equals(0));
-	}
-};
-
+	});
+});
