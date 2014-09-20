8e92c14b45778838cf295308269d8206080eff7b
Martin Delille <martin@phonations.com>
2014-09-20 13:55:39 +0200

    sony spec use bandit

--- specs/SonySpec/SonyControllerSpec.cpp
+++ specs/SonySpec/SonyControllerSpec.cpp
@@ -3,20 +3,21 @@
  * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
  */
 
-#include "igloo/igloo_alt.h"
+#include "bandit/bandit.h"
 
 #include "PhTools/PhDebug.h"
 #include "PhSony/PhSonyController.h"
 
-using namespace igloo;
+using namespace bandit;
 
-Describe(sony_controller_test) {
-	It(compute_rate_from_data_bytes) {
-		AssertThat(PhSonyController::computeRate(0), EqualsWithDelta(0.0f, 0.01f));
-		AssertThat(PhSonyController::computeRate(32), EqualsWithDelta(0.1f, 0.01f));
-		AssertThat(PhSonyController::computeRate(64), EqualsWithDelta(1.0f, 0.01f));
-		AssertThat(PhSonyController::computeRate(79), EqualsWithDelta(2.94f, 0.01f));
-		AssertThat(PhSonyController::computeRate(118), EqualsWithDelta(48.69f, 0.01f));
-	}
-};
-
+go_bandit([](){
+	describe("sony_controller_test", []() {
+		it("compute_rate_from_data_bytes", []() {
+			AssertThat(PhSonyController::computeRate(0), EqualsWithDelta(0.0f, 0.01f));
+			AssertThat(PhSonyController::computeRate(32), EqualsWithDelta(0.1f, 0.01f));
+			AssertThat(PhSonyController::computeRate(64), EqualsWithDelta(1.0f, 0.01f));
+			AssertThat(PhSonyController::computeRate(79), EqualsWithDelta(2.94f, 0.01f));
+			AssertThat(PhSonyController::computeRate(118), EqualsWithDelta(48.69f, 0.01f));
+		});
+	});
+});
