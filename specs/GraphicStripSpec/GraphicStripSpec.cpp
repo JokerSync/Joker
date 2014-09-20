eb82b70552bbfe5677c42003fdb121e19f8b3489
Martin Delille <martin@phonations.com>
2014-09-20 13:01:04 +0200

    graphic strip spec use bandit

--- specs/GraphicStripSpec/GraphicStripSpec.cpp
+++ specs/GraphicStripSpec/GraphicStripSpec.cpp
@@ -5,7 +5,7 @@
 
 #include <QThread>
 
-#include "igloo/igloo_alt.h"
+#include "bandit/bandit.h"
 
 #include "PhTools/PhDebug.h"
 #include "PhTools/PhPictureTools.h"
@@ -15,47 +15,49 @@
 
 #include "GraphicStripSpecSettings.h"
 
-using namespace igloo;
+using namespace bandit;
 
-Describe(graphic_strip_test) {
-	static void SetUpContext() {
-		//PhDebug::disable();
-	}
-
-	It(draw_a_graphic_strip) {
-		PhGraphicView view(980, 320);
-
-		GraphicStripSpecSettings settings;
-		PhGraphicStrip strip(&settings);
-
-		QObject::connect(&view, &PhGraphicView::paint, [&](int w, int h) {
-			strip.draw(0, 0, w, h);
+go_bandit([](){
+	describe("graphic_strip_test", []() {
+		before_each([&](){
+			PhDebug::disable();
 		});
 
-		PhStripDoc * doc = strip.doc();
-		doc->reset();
-		doc->addPeople(new PhPeople("A people"));
-		doc->addPeople(new PhPeople("A second people", "red"));
+		it("draw_a_graphic_strip", [&](){
+			PhGraphicView view(980, 320);
 
-		doc->addObject(new PhStripText(0, doc->peoples().first(), 10000, 0.25f, "Hello", 0.25f));
-		doc->addObject(new PhStripCut(5400, PhStripCut::CrossFade));
-		doc->addObject(new PhStripDetect(PhStripDetect::Off, 0, doc->peoples().first(), 10000, 0.25f));
-		doc->addObject(new PhStripLoop(22000, "label"));
-		doc->addObject(new PhStripText(10000, doc->peoples().last(), 15000, 0.5f, "Hi !", 0.25f));
-		doc->addObject(new PhStripDetect(PhStripDetect::SemiOff, 10000, doc->peoples().last(), 15000, 0.5f));
-		doc->changed();
+			GraphicStripSpecSettings settings;
+			PhGraphicStrip strip(&settings);
 
-		view.show();
+			QObject::connect(&view, &PhGraphicView::paint, [&](int w, int h) {
+				strip.draw(0, 0, w, h);
+			});
 
-		QImage resultImage(view.renderPixmap(980, 320).toImage());
-		QString resultFile = "drawTest.result.bmp";
-		resultImage.save(resultFile);
-		QString expectedFile = "drawTest.expected.bmp";
-		QImage expectedImage(expectedFile);
+			PhStripDoc * doc = strip.doc();
+			doc->reset();
+			doc->addPeople(new PhPeople("A people"));
+			doc->addPeople(new PhPeople("A second people", "red"));
 
-		unsigned int result = PhPictureTools::compare(resultImage, expectedImage, true);
-		PHDEBUG << "result:" << result;
-		AssertThat(result, IsLessThan(920 * 320 * 0.04)); // accept a difference of 4%
-	}
-};
+			doc->addObject(new PhStripText(0, doc->peoples().first(), 10000, 0.25f, "Hello", 0.25f));
+			doc->addObject(new PhStripCut(5400, PhStripCut::CrossFade));
+			doc->addObject(new PhStripDetect(PhStripDetect::Off, 0, doc->peoples().first(), 10000, 0.25f));
+			doc->addObject(new PhStripLoop(22000, "label"));
+			doc->addObject(new PhStripText(10000, doc->peoples().last(), 15000, 0.5f, "Hi !", 0.25f));
+			doc->addObject(new PhStripDetect(PhStripDetect::SemiOff, 10000, doc->peoples().last(), 15000, 0.5f));
+			doc->changed();
+
+			view.show();
+
+			QImage resultImage(view.renderPixmap(980, 320).toImage());
+			QString resultFile = "drawTest.result.bmp";
+			resultImage.save(resultFile);
+			QString expectedFile = "drawTest.expected.bmp";
+			QImage expectedImage(expectedFile);
+
+			unsigned int result = PhPictureTools::compare(resultImage, expectedImage, true);
+			PHDEBUG << "result:" << result;
+			AssertThat(result, IsLessThan(920 * 320 * 0.04)); // accept a difference of 4%
+		});
+	});
+});
 
