f9efa3567813b8e7a0bd268cd580f583ea7019ca
Timothee Lecomte <lecomte.timothee@gmail.com>
2014-09-22 10:18:06 +0200

    (PhVideoEngine) Fix swsContext leak
    sws_getContext would allocate a new context on each frame. This commit
    actually reverts to the previous behaviour before 35ac31e.

--- libs/PhVideo/PhVideoEngine.cpp
+++ libs/PhVideo/PhVideoEngine.cpp
@@ -14,6 +14,7 @@ PhVideoEngine::PhVideoEngine(PhVideoSettings *settings) :
 	_pFormatContext(NULL),
 	_videoStream(NULL),
 	_videoFrame(NULL),
+	_swsContext(NULL),
 	_currentFrame(PHFRAMEMIN),
 	_useAudio(false),
 	_audioStream(NULL),
@@ -156,6 +157,11 @@ void PhVideoEngine::close()
 	if(_rgb) {
 		delete _rgb;
 		_rgb = NULL;
+	}
+
+	if (_swsContext) {
+		sws_freeContext(_swsContext);
+		_swsContext = NULL;
 	}
 
 	if(_pFormatContext) {
@@ -317,14 +323,16 @@ bool PhVideoEngine::decodeFrame(PhFrame frame)
 						/* Note: we output the frames in AV_PIX_FMT_BGRA rather than AV_PIX_FMT_RGB24,
 						 * because this format is native to most video cards and will avoid a conversion
 						 * in the video driver */
-						SwsContext * swsContext = sws_getContext(_videoFrame->width, _videoStream->codec->height, pixFormat,
+						/* sws_getCachedContext will check if the context is valid for the given parameters. It the context is not valid,
+						 * it will be freed and a new one will be allocated. */
+						_swsContext = sws_getCachedContext(_swsContext, _videoFrame->width, _videoStream->codec->height, pixFormat,
 						                                         _videoStream->codec->width, frameHeight, AV_PIX_FMT_BGRA,
 						                                         SWS_POINT, NULL, NULL, NULL);
 
 						if(_rgb == NULL)
 							_rgb = new uint8_t[avpicture_get_size(AV_PIX_FMT_BGRA, _videoFrame->width, frameHeight)];
 						int linesize = _videoFrame->width * 4;
-						if (0 <= sws_scale(swsContext, (const uint8_t * const *) _videoFrame->data,
+						if (0 <= sws_scale(_swsContext, (const uint8_t * const *) _videoFrame->data,
 						                   _videoFrame->linesize, 0, _videoStream->codec->height, &_rgb,
 						                   &linesize)) {
 
