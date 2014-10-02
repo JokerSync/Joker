eb3c8634a44a3a168e6cdde80f490ee95c902d30
Martin Delille <martin@phonations.com>
2014-10-02 18:02:29 +0200

    add Window specs

---
+++ specs/CommonUISpec/WindowSpecSettings.h
@@ -0,0 +1,27 @@
+#ifndef WINDOWSPECSETTINGS_H
+#define WINDOWSPECSETTINGS_H
+
+#include <QDir>
+
+#include "PhTools/PhGenericSettings.h"
+#include "PhCommonUI/PhDocumentWindowSettings.h"
+
+class WindowSpecSettings : public PhGenericSettings, public PhDocumentWindowSettings
+{
+public:
+	WindowSpecSettings(bool clear = false) : PhGenericSettings(clear) {
+	}
+
+	// PhWindowSettings
+	//PH_SETTING_BOOL(setFullScreen, fullScreen) // already implemented in PhWindowSettings
+	PH_SETTING_BYTEARRAY(setWindowGeometry, windowGeometry)
+
+	// PhDocumentWindowSettings
+	PH_SETTING_STRING(setCurrentDocument, currentDocument)
+	PH_SETTING_STRING2(setLastDocumentFolder, lastDocumentFolder, QDir::homePath())
+	PH_SETTING_STRINGLIST(setRecentDocumentList, recentDocumentList)
+	PH_SETTING_INT2(setMaxRecentDocument, maxRecentDocument, 5)
+	PH_SETTING_BOOL2(setAutoReload, autoReload, true)
+};
+
+#endif // WINDOWSPECSETTINGS_H
---
+++ specs/CommonUISpec/WindowSpecWindow.cpp
@@ -0,0 +1,49 @@
+#include <QPlainTextEdit>
+
+#include "PhTools/PhDebug.h"
+
+#include "WindowSpecWindow.h"
+#include "ui_WindowSpecWindow.h"
+
+WindowSpecWindow::WindowSpecWindow(PhDocumentWindowSettings *settings) :
+	PhDocumentWindow(settings),
+	ui(new Ui::WindowSpecWindow)
+{
+	ui->setupUi(this);
+
+	connect(ui->actionFull_screen, SIGNAL(triggered()), this, SLOT(toggleFullScreen()));
+}
+
+WindowSpecWindow::~WindowSpecWindow()
+{
+	delete ui;
+}
+
+QAction *WindowSpecWindow::fullScreenAction()
+{
+	return ui->actionFull_screen;
+}
+
+bool WindowSpecWindow::openDocument(QString fileName)
+{
+	PHDEBUG << fileName;
+	QFile file(fileName);
+	if(!file.open(QFile::ReadOnly))
+		return false;
+
+	QTextStream ts(&file);
+	ui->plainTextEdit->setPlainText(ts.readAll());
+
+	setCurrentDocument(fileName);
+	return true;
+}
+
+QMenu *WindowSpecWindow::recentDocumentMenu()
+{
+	return ui->menuOpen_recent;
+}
+
+QString WindowSpecWindow::text()
+{
+	return ui->plainTextEdit->toPlainText();
+}
