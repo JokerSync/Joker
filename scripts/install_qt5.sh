f63db447fef35a1cb3e0c5e987fefcfdce6976e6
Martin Delille <martin@phonations.com>
2015-05-18 17:30:56 +0200

    disable ubuntu ppaZ

--- scripts/install_qt5.sh
+++ scripts/install_qt5.sh
@@ -9,8 +9,8 @@ brew install qt5
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 echo "Linux detected"
 
-yes | sudo add-apt-repository ppa:ubuntu-sdk-team/ppa
-sudo apt-get update -qq
+#yes | sudo add-apt-repository ppa:ubuntu-sdk-team/ppa
+#sudo apt-get update -qq
 sudo apt-get install build-essential g++ curl
 sudo apt-get install -qq qt5-default qtdeclarative5-dev
 
