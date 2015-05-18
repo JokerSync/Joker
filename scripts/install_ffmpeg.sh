7ff0c2a8a39ddcad5d99743025a05579434ad71f
Martin Delille <martin@phonations.com>
2015-05-18 17:36:52 +0200

    use deb multimedia ppa

--- scripts/install_ffmpeg.sh
+++ scripts/install_ffmpeg.sh
@@ -10,7 +10,9 @@ brew install libav ffmpeg
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 echo "Linux detected"
 
-sudo apt-get install build-essential yasm ffmpeg2theora
+sudo echo deb http://www.deb-multimedia.org testing main non-free >> /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install ffmpeg
 #curl -L http://ffmpeg.org/releases/ffmpeg-2.6.2.tar.bz2 -o ffmpeg-2.6.2.tar.bz2 
 #curl -L http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz -o ffmpeg-release-64bit-static.tar.xz
 #tar -xvjf ffmpeg-release-64bit-static.tar.xz
