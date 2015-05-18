64c8bea0c42f31d8d8ccfe753b195208d5b85d53
Martin Delille <martin@debian.vm.phonations.com>
2015-05-18 15:19:25 +0200

    install ffmpeg using ffmpeg2theora

--- scripts/install_ffmpeg.sh
+++ scripts/install_ffmpeg.sh
@@ -10,16 +10,17 @@ brew install libav ffmpeg
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 echo "Linux detected"
 
-sudo apt-get install yasm
-curl -L http://ffmpeg.org/releases/ffmpeg-2.6.2.tar.bz2 -o ffmpeg-2.6.2.tar.bz2 
-tar -xvjf ffmpeg-2.6.2.tar.bz2 
-cd ffmpeg-2.6.2
-./configure
-make
-sudo make install
-cd ..
-rm ffmpeg-2.6.2.tar.bz2 
-rm -rf ffmpeg-2.6.2
+sudo apt-get install build-essential yasm ffmpeg2theora
+#curl -L http://ffmpeg.org/releases/ffmpeg-2.6.2.tar.bz2 -o ffmpeg-2.6.2.tar.bz2 
+#curl -L http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz -o ffmpeg-release-64bit-static.tar.xz
+#tar -xvjf ffmpeg-release-64bit-static.tar.xz
+#cd ffmpeg-release-64bit-static
+#./configure
+#make
+#sudo make install
+#cd ..
+#rm -rf ffmpeg-release-64bit-static
+#rm ffmpeg-release-64bit-static.tar.xz
 
 fi
 
