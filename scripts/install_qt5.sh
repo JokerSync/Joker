8b3fa8be9b25e196a9b35c5fffffd86420b80653
Martin Delille <martin@phonations.com>
2015-05-19 13:56:00 +0200

    search for qt5

--- scripts/install_qt5.sh
+++ scripts/install_qt5.sh
@@ -10,8 +10,9 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 echo "Linux detected"
 
 #yes | sudo add-apt-repository ppa:ubuntu-sdk-team/ppa
-#sudo apt-get update -qq
-sudo apt-get install build-essential g++ curl
-sudo apt-get install libqt5core5a libqt5gui5 libqt5opengl5
+sudo apt-get update 
+sudo apt-cache search qt5
+#sudo apt-get install build-essential g++ curl
+#sudo apt-get install libqt5core5a libqt5gui5 libqt5opengl5
 
 fi
