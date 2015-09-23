62ac0a69aaf47acffe42e4e5981d64d33a8ba528
Martin Delille <martin@phonations.com>
2015-09-23 15:16:10 +0200

    better qt5 install

--- scripts/install_ffmpeg.sh
+++ scripts/install_ffmpeg.sh
@@ -10,19 +10,11 @@ brew install libav ffmpeg
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 echo "Linux detected"
 
-sudo echo deb http://www.deb-multimedia.org testing main non-free >> /etc/apt/sources.list
+#sudo echo deb http://www.deb-multimedia.org testing main non-free >> /etc/apt/sources.list
+#udo add-apt-repository ppa:mc3man/trusty-media
 sudo apt-get update
-sudo apt-get install ffmpeg
-#curl -L http://ffmpeg.org/releases/ffmpeg-2.6.2.tar.bz2 -o ffmpeg-2.6.2.tar.bz2 
-#curl -L http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz -o ffmpeg-release-64bit-static.tar.xz
-#tar -xvjf ffmpeg-release-64bit-static.tar.xz
-#cd ffmpeg-release-64bit-static
-#./configure
-#make
-#sudo make install
-#cd ..
-#rm -rf ffmpeg-release-64bit-static
-#rm ffmpeg-release-64bit-static.tar.xz
+sudo apt-get dist-upgrade
+sudo apt-get install libavformat-dev libswscale-dev
 
 fi
 
