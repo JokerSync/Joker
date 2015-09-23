f680959551bbcac40a02fa936c5371e34d06cc8f
Martin Delille <martin@phonations.com>
2015-09-23 16:51:25 +0200

    add boost

--- scripts/install_qt5.sh
+++ scripts/install_qt5.sh
@@ -12,6 +12,7 @@ echo "Linux detected"
 sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
 sudo apt-get install qt5-qmake qtbase5-dev qtdeclarative5-dev
+sudo apt-get install libboost-all-dev
 #sudo apt-get install libqt5core5a libqt5gui5 libqt5opengl5
 
 fi
