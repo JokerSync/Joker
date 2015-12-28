483a004f8bec41e036f0bd3cca160fcf51e9b505
Martin Delille <martin@phonations.com>
2015-12-28 17:56:58 +0100

    separate portaudio and rtmidi installation script

--- scripts/install_portaudio.sh
+++ scripts/install_portaudio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Install audio"
+echo "Install portaudio"
 if [ "$(uname)" == "Darwin" ]; then
 echo "Mac OS X detected"
 brew install portaudio
@@ -8,7 +8,6 @@ brew install portaudio
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 echo "Linux detected"
 
-echo "Install portaudio"
 sudo apt-get install yasm
 curl -L http://portaudio.com/archives/pa_stable_v19_20140130.tgz -o pa_stable_v19_20140130.tgz
 tar -xvzf pa_stable_v19_20140130.tgz
@@ -19,20 +18,6 @@ sudo make install
 cd ..
 rm pa_stable_v19_20140130.tgz
 rm -rf portaudio
-
-sudo apt-get install libasound2-dev
-
-echo "Install RtMidi"
-cd vendor/rtmidi
-sudo apt-get install autoconf libtool
-#libtoolize
-#aclocal
-#automake --add-missing
-autoconf
-./configure
-make
-sudo make install
-cd ../..
 
 fi
 
