382715f0ca47ceac777a1d496e8263188826802e
Martin Delille <martin@phonations.com>
2015-12-31 15:56:45 +0100

    clean install_ffmpeg

--- scripts/install_ffmpeg.sh
+++ scripts/install_ffmpeg.sh
@@ -10,10 +10,6 @@ brew install libav ffmpeg
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 echo "Linux detected"
 
-#sudo echo deb http://www.deb-multimedia.org testing main non-free >> /etc/apt/sources.list
-#udo add-apt-repository ppa:mc3man/trusty-media
-#sudo apt-get update -qy
-#sudo apt-get dist-upgrade -qy
 sudo apt-get install libavformat-dev libswscale-dev
 
 fi
